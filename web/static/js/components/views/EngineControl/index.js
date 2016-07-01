66d966b9c9707ba96cf4d7078cb6d387932c1187
Alex Anderson <alexanderson1993@gmail.com>
2016-06-30 18:39:25 -0600

    Engine control card view is basically done, minus a mechanism for flushing coolant.

--- web/static/js/components/views/EngineControl/index.js
+++ web/static/js/components/views/EngineControl/index.js
@@ -7,6 +7,15 @@ import { connect } from 'react-redux';
 
 import './style.scss';
 
+const HeatBar = (props) => {
+	return (<div>
+		<label className="heatBox-Label">{props.label}</label>
+		<div className="heatBox">
+		<div className="heatBar" style={{height:`${props.level}%`,backgroundImage:props.background}}></div>
+		</div>
+		</div>);
+};
+
 class EngineControlContent extends Component {
 	componentDidMount() {
 		let { dispatch } = this.props;
@@ -30,7 +39,6 @@ class EngineControlContent extends Component {
 			}
 		}
 		if (index < engines.length - 1){
-			debugger;
 			//More than one engine, update the next ones to be zero
 			for (let i = index + 1; i <= engines.length - 1; i++){
 				let newObj = {speed: -1};
@@ -46,26 +54,100 @@ class EngineControlContent extends Component {
 			<Row>
 			<Col className="col-sm-12 enginesBar">
 			{(() => {
-				//if (engines.length === 1){
-					return engines.map((engine, index, array) => {
-						return (
-							<div className="engineGroup">
-							<h4>{engine.name}</h4>
-							<ul className="engine">
-							{ engine.speeds.map((speed, speedIndex) => {
-								return (
-									<li className="speedNums speedBtn" onClick={() => {this.setSpeed(engine,speedIndex,engines,index)}}>{speed}</li>
-									);
-							})}
-							</ul>
-							<div className="speedBar" style={this.speedBarStyle(engine.speeds,engine.speed)}></div>
-							</div>
-							);
-					});
-				//}
-				//if (engines.length > 1){
+				return engines.map((engine, index, array) => {
+					return (
+						<div className="engineGroup">
+						<h4>{engine.name}</h4>
+						<ul className="engine">
+						{ engine.speeds.map((speed, speedIndex) => {
+							let speedWord = speed;
+							if (typeof speed === "object"){
+								speedWord = speed.number;
+							}
+							return (
+								<li className="speedNums speedBtn" onClick={() => {this.setSpeed(engine,speedIndex,engines,index);}}>{speedWord}</li>
+								);
+						})}
+						</ul>
+						<div className="speedBar" style={this.speedBarStyle(engine.speeds,engine.speed)}></div>
+						</div>
+						);
+				});
+			})()}
+			</Col>
+			</Row>
+			<Row>
+			<Col className="col-sm-12">
+			{(() => {
+				if (engines.length === 1){
+					return (
+						<div>
+						<Col className="col-sm-4">
+						{engines[0].speeds.map((speed, speedIndex) => {
+							let speedWord = speed;
+							if (typeof speed === "object"){
+								speedWord = speed.text;
+							}
+							return <button class="btn btn-primary speedBtn">{speedWord}</button>;
+						})}
+						</Col>
+						<Col className="col-sm-2">
+						<Col className="col-sm-6">
+						<HeatBar label="Heat" background="linear-gradient(to bottom, #440000 0%,#ff0000 50%,#440000 100%)" level={engines[0].heat}/>
+						</Col>
+						<Col className="col-sm-6">
+						<HeatBar label="Coolant" background="linear-gradient(to bottom, #004400 0%,#00ff00 50%,#004400 100%)" level={engines[0].coolant}/>
+						</Col>
+						</Col>
+						<Col className="col-sm-6">
 
-				//}
+						</Col>
+						</div>
+						);
+				}
+				if (engines.length === 2){
+					return (
+						<div>
+						<Col className="col-sm-2">
+						{engines[0].speeds.map((speed, speedIndex) => {
+							let speedWord = speed;
+							if (typeof speed === "object"){
+								speedWord = speed.text;
+							}
+							return <Button type="primary" className="btn-block speedBtn" onClick={() => {this.setSpeed(engines[0],speedIndex,engines,0);}} label={speedWord} />;
+						})}
+						</Col>
+						<Col className="col-sm-2">
+						<Col className="col-sm-6">
+						<HeatBar label="Heat" background="linear-gradient(to bottom, #440000 0%,#ff0000 50%,#440000 100%)" level={engines[0].heat}/>
+						</Col>
+						<Col className="col-sm-6">
+						<HeatBar label="Coolant" background="linear-gradient(to bottom, #004400 0%,#00ff00 50%,#004400 100%)" level={engines[0].coolant}/>
+						</Col>
+						</Col>
+						<Col className="col-sm-4">
+
+						</Col>
+						<Col className="col-sm-2">
+						<Col className="col-sm-6">
+						<HeatBar label="Heat" background="linear-gradient(to bottom, #440000 0%,#ff0000 50%,#440000 100%)" level={engines[1].heat}/>
+						</Col>
+						<Col className="col-sm-6">
+						<HeatBar label="Coolant" background="linear-gradient(to bottom, #004400 0%,#00ff00 50%,#004400 100%)" level={engines[1].coolant}/>
+						</Col>
+						</Col>
+						<Col className="col-sm-2">
+						{engines[1].speeds.map((speed, speedIndex) => {
+							let speedWord = speed;
+							if (typeof speed === "object"){
+								speedWord = speed.text;
+							}
+							return <Button type="primary" className="btn-block speedBtn" onClick={() => {this.setSpeed(engines[1],speedIndex,engines,1);}} label={speedWord} />;
+						})}
+						</Col>
+						</div>
+						);
+				}
 			})()}
 			</Col>
 			</Row>
