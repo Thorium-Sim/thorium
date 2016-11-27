8b313fe55f5611795773d5484dce44b1031ed137
Alex Anderson <aanderson@blenderbottle.com>
2016-11-26 23:32:31 -0700

    Continuing work on the transporters.

--- src/components/views/Transporters/index.js
+++ src/components/views/Transporters/index.js
@@ -1,103 +1,41 @@
 import React, {Component} from 'react';
 import gql from 'graphql-tag';
 import { Button, Row, Col, Input } from 'reactstrap';
-import { graphql, compose } from 'react-apollo';
-import Draggable from 'react-draggable';
-
+import { graphql, withApollo } from 'react-apollo';
+import Target from './targeting';
 import Scan from './transporterScan';
 import './style.scss';
 
-const targets = [
-{
-  id: "1234",
-  icon: "Triangle",
-  moving: false,
-  coordinates: {
-    x: Math.random(),
-    y: Math.random(),
-    z: Math.random(),
-  }
-}
-];
-
-const ChargeBar = (props) => {
-  return (
-    <div className="chargeBar" style={{height: `${props.charge * 100}%`}} />
-    );
-}
-class Target extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      selectedTarget: null,
+const TRANSPORTER_SUB = gql`
+subscription TransportersSub{
+  transporterUpdate {
+    id
+    type
+    state
+    charge
+    simulatorId
+    targets {
+      id
+      icon
+      moving
     }
+    requestedTarget
+    destination
   }
-  render(){
-    return (
-      <div>
-      <Row>
-      <h2 style={{color: 'yellow', width: '100%', textAlign: 'center', opacity:this.state.selectedTarget ? 1 : 0}}>Transport Possible</h2>
-      </Row>
-      <Row>
-      <Col className="targetBox" sm={{size: 4, offset: 1}}>
-      {this.props.targets.map((target) => {
-        return <img
-        draggable="false"
-        role="presentation"
-        src={require('./crosstarget.svg')}
-        style={{position: 'absolute', left: `${target.coordinates.x * 90}%`, top: `${target.coordinates.y * 90}%`}} />
-      })}
-      <Draggable
-      bounds=".targetBox"
-      onDrag={(event,obj) => {
-        const {clientWidth, clientHeight} = event.target.parentElement;
-        const {x, y} = obj;
-        let selectedTarget = null
-        this.props.targets.forEach((target) => {
-          const {x: objX, y: objY} = this.props.targets[0].coordinates;
-          if (Math.round((objX - x/clientWidth*1.11111) * 100) === 0 && Math.round((objY - y/clientHeight*1.11111) * 100) === 0){
-            // The crosshair is on top of a target
-            selectedTarget = target;
-          }
-        })
-        this.setState({
-          selectedTarget,
-        })
-      }}
-      >
-      <img draggable="false" role="presentation" src={require('./crosshairs.svg')} />
-      </Draggable>
-
-      </Col>
-      <Col className="chargeBox" sm={{size: 4, offset: 2}}>
-      <ChargeBar charge={0.5} />
-      <ChargeBar charge={0.5} />
-      <ChargeBar charge={0.5} />
-      </Col>
-      </Row>
-      <Row>
-      <Col sm={{size: 4, offset: 4}}>
-      <div style={{height: '30px'}} />
-      <Button block color={'warning'}>Cancel Transport</Button>
-      </Col>
-      </Row>
-      </div>
-      );
-  }
-}
+}`;
 
 const TargetSelect = (props) => {
   return (
     <Col sm={{size:6, offset:3}}>
     <div style={{height: '60px'}} />
     <h3>Enter Target:</h3>
-    <Input placeholder="Enter Target..." size="lg" />
+    <Input defaultValue={props.target} onBlur={props.updateTarget} placeholder="Enter Target..." size="lg" />
     <div style={{height: '60px'}} />
     <h3>Enter Destination:</h3>
-    <Input placeholder="Enter Destination..." size="lg" />
+    <Input defaultValue={props.destination} onBlur={props.updateDestination} placeholder="Enter Destination..." size="lg" />
     <div style={{height: '30px'}} />
     <Col sm={{size: 6, offset: 3}}>
-    <Button block color={'primary'}>Begin Scan</Button>
+    <Button block color={'primary'} onClick={props.beginScan}>Begin Scan</Button>
     </Col>
     </Col>
     );
@@ -108,7 +46,7 @@ const Scanning = (props) => {
     <Scan />
     <h3 style={{textAlign: 'center', width: '100%'}}>Scanning...</h3>
     <Col sm={{size: 6, offset: 3}}>
-    <Button block color={'primary'} size="lg">Cancel Scan</Button>
+    <Button block color={'primary'} size="lg" onClick={props.cancelScan}>Cancel Scan</Button>
     </Col>
     </Col>
     );
@@ -117,11 +55,99 @@ const Scanning = (props) => {
 class Transporters extends Component {
   constructor(props) {
     super(props);
+    this.transporterSubscription = null;
+  }
+  componentWillReceiveProps(nextProps) {
+    if (!this.transporterSubscription && !nextProps.data.loading) {
+      this.transporterSubscription = nextProps.data.subscribeToMore({
+        document: TRANSPORTER_SUB,
+        updateQuery: (previousResult, {subscriptionData}) => {
+          previousResult.transporters = previousResult.transporters.map(transporter => {
+            if (transporter.id === subscriptionData.data.transporterUpdate.id){
+              transporter = subscriptionData.data.transporterUpdate
+            } 
+            return transporter;
+          })
+          return previousResult;
+        },
+      });
+    }
+  }
+  updateTarget(transporter, e){
+    this.props.client.mutate({
+      mutation: gql`
+      mutation SetTransporterTarget($transporter: ID!, $target: String!){
+        setTransportTarget(transporter: $transporter, target: $target)
+      }`,
+      variables: {
+        transporter: transporter.id,
+        target: e.target.value
+      }
+    })
+  }
+  updateDestination(transporter, e){
+    this.props.client.mutate({
+      mutation: gql`
+      mutation SetTransporterDestination($transporter: ID!, $destination: String!){
+        setTransportDestination(transporter: $transporter, destination: $destination)
+      }`,
+      variables: {
+        transporter: transporter.id,
+        destination: e.target.value
+      }
+    })
+  }
+  beginScan(transporter){
+    console.log(transporter);
+    this.props.client.mutate({
+      mutation: gql`
+      mutation BeginTransportScan($transporter: ID!){
+        beginTransportScan(transporter: $transporter)
+      }`,
+      variables: {
+        transporter: transporter.id,
+      }
+    })
+  }
+  cancelScan(transporter){
+    this.props.client.mutate({
+      mutation: gql`
+      mutation CancelTransporterScan($transporter: ID!){
+        cancelTransportScan(transporter: $transporter)
+      }`,
+      variables: {
+        transporter: transporter.id,
+      }
+    })
+  }
+  cancelTransport(transporter){
+    this.props.client.mutate({
+      mutation: gql`
+      mutation CancelTransport($transporter: ID!){
+        clearTransportTargets(transporter: $transporter)
+      }`,
+      variables: {
+        transporter: transporter.id,
+      }
+    })
   }
   render() {
+    // Assume that there is only one transporter
+    let transporter = {};
+    if (!this.props.data.loading){
+      transporter = this.props.data.transporters[0] || {};
+    }
+    console.log(this.props.data);
     return (
       <Row>
-      <Target targets={targets} />
+      {
+        this.props.data.loading ? "Loading..." :
+        <div>
+        {transporter.state === 'Inactive' && <TargetSelect beginScan={this.beginScan.bind(this, transporter)} updateTarget={this.updateTarget.bind(this, transporter)} updateDestination={this.updateDestination.bind(this, transporter)} target={transporter.requestedTarget} destination={transporter.destination} />}
+        {transporter.state === 'Scanning' && <Scanning cancelScan={this.cancelScan.bind(this, transporter)} />}
+        {(transporter.state === 'Targeting' || transporter.state === 'Charging') && <Target cancelTransport={this.cancelTransport.bind(this, transporter)} targets={transporter.targets} />}
+        </div>
+      }
       </Row>
       );
   }
@@ -147,4 +173,4 @@ query GetTransporters($simulatorId: ID){
 `;
 export default  graphql(TRANSPORTERS_QUERY, {
   options: (props) => ({ variables: { simulatorId: 'test' } }),
-})(Transporters);
+})(withApollo(Transporters));
---
+++ src/components/views/Transporters/targeting.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import Draggable from 'react-draggable';
+import { Button, Row, Col } from 'reactstrap';
+
+const ChargeBar = (props) => {
+  return (
+    <div className="chargeBar" style={{height: `${props.charge * 100}%`}} />
+    );
+}
+
+export default class Target extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedTarget: null,
+    }
+  }
+  render(){
+    return (
+      <div>
+      <Row>
+      <h2 style={{color: 'yellow', width: '100%', textAlign: 'center', opacity:this.state.selectedTarget ? 1 : 0}}>Transport Possible</h2>
+      </Row>
+      <Row>
+      <Col className="targetBox" sm={{size: 4, offset: 1}}>
+      {this.props.targets.map((target) => {
+        return <img
+        key={target.id}
+        draggable="false"
+        role="presentation"
+        src={require('./crosstarget.svg')}
+        style={{position: 'absolute', left: `${target.coordinates.x * 90}%`, top: `${target.coordinates.y * 90}%`}} />
+      })}
+      <Draggable
+      bounds=".targetBox"
+      onDrag={(event,obj) => {
+        const {clientWidth, clientHeight} = event.target.parentElement;
+        const {x, y} = obj;
+        let selectedTarget = null
+        this.props.targets.forEach((target) => {
+          const {x: objX, y: objY} = this.props.targets[0].coordinates;
+          if (Math.round((objX - x/clientWidth*1.11111) * 100) === 0 && Math.round((objY - y/clientHeight*1.11111) * 100) === 0){
+            // The crosshair is on top of a target
+            selectedTarget = target;
+          }
+        })
+        this.setState({
+          selectedTarget,
+        })
+      }}
+      >
+      <img draggable="false" role="presentation" src={require('./crosshairs.svg')} />
+      </Draggable>
+
+      </Col>
+      <Col className="chargeBox" sm={{size: 4, offset: 2}}>
+      <ChargeBar charge={0.5} />
+      <ChargeBar charge={0.5} />
+      <ChargeBar charge={0.5} />
+      </Col>
+      </Row>
+      <Row>
+      <Col sm={{size: 4, offset: 4}}>
+      <div style={{height: '30px'}} />
+      <Button block color={'warning'} onClick={this.props.cancelTransport}>Cancel Transport</Button>
+      </Col>
+      </Row>
+      </div>
+      );
+  }
+}
