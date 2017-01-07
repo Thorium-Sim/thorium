6a9026cf9308c93d273a26e7aea90806b6c9c58f
Alex Anderson <alexanderson1993@gmail.com>
2017-01-07 14:41:12 -0700

    Issue the event directly instead of using a mutation.

--- processes/engines.js
+++ processes/engines.js
@@ -1,12 +1,5 @@
-import gql from 'graphql-tag';
-import request from 'request';
 import App from '../app';
 
-const mutation = gql`
-mutation HeatIncrement($id: ID, $heat: Float){
-  addHeat(id: $id, heat: $heat)
-}
-`;
 const updateHeat = () => {
   App.systems.forEach((sys) => {
     if (sys.type === 'Engine') {
@@ -14,17 +7,7 @@ const updateHeat = () => {
       const heatAdd = speedVal * sys.heatRate;
       const sysHeat = sys.heat;
       if (sysHeat + heatAdd >= 0 && sysHeat + heatAdd <= 100 && heatAdd !== 0) {
-        request.post(
-          'http://localhost:3001/graphql',
-          {
-            json: {
-              query: mutation,
-              operationName: 'HeatIncrement',
-              variables: { id: sys.id, heat: heatAdd },
-            },
-          },
-          () => {}
-          );
+        App.handleEvent({ id: sys.id, heat: heatAdd }, 'addHeat', 'addedHeat');
       }
     }
   });
