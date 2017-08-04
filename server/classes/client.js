f1e046bab25ece3fabcca7726319e6003c386cb1
Alex Anderson <alexanderson1993@gmail.com>
2017-08-04 16:41:38 -0600

    Trying to learn flow typing, so I set it up in Thorium and added a few types. Looks like it will be really valuable

--- server/classes/client.js
+++ server/classes/client.js
@@ -1,20 +1,44 @@
-import uuid from 'uuid';
+// @flow
+import uuid from "uuid";
 
 // TODO: Add the ability to sort clients into groups
 
 export default class Client {
-  constructor(params = {}) {
+  id: string;
+  flightId: ?string;
+  simulatorId: ?string;
+  station: ?string;
+  loginName: ?string;
+  loginState: string;
+  sentPing: ?boolean;
+  ping: ?string;
+  connected: ?boolean;
+  offlineState: ?string;
+  class: string;
+  constructor(params: {
+    id: ?string,
+    flightId: ?string,
+    simulatorId: ?string,
+    station: ?string,
+    loginName: ?string,
+    loginState: ?string,
+    sentPing: ?boolean,
+    ping: ?string,
+    connected: ?boolean,
+    offlineState: ?string
+  }) {
+    params = params || {};
     this.id = params.id || uuid.v4();
     this.flightId = params.flightId || null;
     this.simulatorId = params.simulatorId || null;
     this.station = params.station || null;
     this.loginName = params.loginName || null;
-    this.loginState = params.loginState || 'logout';
+    this.loginState = params.loginState || "logout";
     this.sentPing = null;
     this.ping = null;
     this.connected = params.connected || false;
     this.offlineState = params.offlineState || null;
-    this.class = 'Client';
+    this.class = "Client";
   }
   connect() {
     this.connected = true;
@@ -22,40 +46,32 @@ export default class Client {
   disconnect() {
     this.connected = false;
   }
-  setPing(ping) {
+  setPing(ping: boolean) {
     this.sentPing = ping;
   }
-  setFlight(flightId) {
+  setFlight(flightId: string) {
     this.flightId = flightId;
   }
-  setSimulator(simulatorId) {
+  setSimulator(simulatorId: string) {
     this.simulatorId = simulatorId;
   }
-  setStation(station) {
+  setStation(station: string) {
     this.station = station;
   }
-  login(name) {
+  login(name: string) {
     this.loginName = name;
-    this.loginState = 'login';
+    this.loginState = "login";
   }
   logout() {
     this.loginName = null;
-    this.loginState = 'logout';
+    this.loginState = "logout";
   }
-  setOfflineState(state) {
+  setOfflineState(state: string) {
     // Allow one of null, 'blackout', 'offline', 'power', 'lockdown', and 'maintenance'
     this.offlineState = state;
   }
-  diagnostic() {
-
-  }
-  reset() {
-
-  }
-  lockScreen() {
-
-  }
-  unlockScreen() {
-
-  }
+  diagnostic() {}
+  reset() {}
+  lockScreen() {}
+  unlockScreen() {}
 }
