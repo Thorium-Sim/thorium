aa1b437133e594f356408582a86bec6e72ae9e63
Alex Anderson <alexanderson1993@gmail.com>
2017-08-05 08:02:20 -0600

    Fixes for Transporters

--- src/components/views/Transporters/core.js
+++ src/components/views/Transporters/core.js
@@ -1,96 +1,135 @@
-import React, {Component} from 'react';
-import { InputField, OutputField } from '../../generic/core';
-import { graphql, withApollo } from 'react-apollo';
-import gql from 'graphql-tag';
+import React, { Component } from "react";
+import { InputField, OutputField } from "../../generic/core";
+import { graphql, withApollo } from "react-apollo";
+import gql from "graphql-tag";
 
 const TRANSPORTER_SUB = gql`
-subscription TransportersSub{
-  transporterUpdate {
-    id
-    type
-    state
-    charge
-    simulatorId
-    targets {
+  subscription TransportersSub {
+    transporterUpdate {
       id
-      icon
-      moving
-      position {
-        x
-        y
+      type
+      state
+      charge
+      simulatorId
+      targets {
+        id
+        icon
+        moving
+        position {
+          x
+          y
+        }
       }
+      requestedTarget
+      destination
     }
-    requestedTarget
-    destination
   }
-}`;
+`;
 
 class TransporterCore extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.transporterSubscription = null;
   }
+  state = {};
   componentWillReceiveProps(nextProps) {
     if (!this.transporterSubscription && !nextProps.data.loading) {
       this.transporterSubscription = nextProps.data.subscribeToMore({
         document: TRANSPORTER_SUB,
-        updateQuery: (previousResult, {subscriptionData}) => {
-          previousResult.transporters = previousResult.transporters.map(transporter => {
-            if (transporter.id === subscriptionData.data.transporterUpdate.id){
-              transporter = subscriptionData.data.transporterUpdate
-            } 
+        updateQuery: (previousResult, { subscriptionData }) => {
+          const transporters = previousResult.transporters.map(transporter => {
+            if (transporter.id === subscriptionData.data.transporterUpdate.id) {
+              transporter = subscriptionData.data.transporterUpdate;
+            }
             return transporter;
-          })
-          return previousResult;
-        },
+          });
+          return Object.assign({}, previousResult, { transporters });
+        }
       });
     }
+    if (this.props.data.transporters && this.props.data.transporters[0]) {
+      if (
+        this.props.data.transporters[0].state === "Charging" &&
+        (nextProps.data.transporters[0].state === "Targeting" ||
+          nextProps.data.transporters[0].state === "Inactive")
+      ) {
+        this.setState({
+          transported: true
+        });
+        setTimeout(() => this.setState({ transported: false }), 4000);
+      }
+    }
   }
-  targets(transporter, result){
+  targets(transporter, result) {
     this.props.client.mutate({
       mutation: gql`
-      mutation SetTransporterTargets($transporter: ID!, $targets: Int!){
-        setTransporterTargets(transporter: $transporter, targets: $targets)
-      }`,
+        mutation SetTransporterTargets($transporter: ID!, $targets: Int!) {
+          setTransporterTargets(transporter: $transporter, targets: $targets)
+        }
+      `,
       variables: {
         transporter: transporter.id,
         targets: result
       }
-    })
+    });
   }
-  render(){
-    const transporter = this.props.data.loading ? {targets:[]} : this.props.data.transporters[0];
-    return (<div>
-      <p>Transporters</p>
-      {this.props.data.loading ? <span>Loading...</span> : 
-        (this.props.data.transporters.length > 0 ? <div>
-        <OutputField alert={transporter.state === 'Scanning'}>{transporter.state} {transporter.state === 'Charging' && `- ${Math.round(transporter.charge * 100)}%`}</OutputField>
-        <OutputField>{transporter.requestedTarget}</OutputField>
-        <OutputField>{transporter.destination}</OutputField>
-        <InputField prompt="How many transporter targets?" onClick={this.targets.bind(this, transporter)}>{transporter.targets.length}</InputField>
-        </div> : "No transporters")
-      }
-      </div>);
+  render() {
+    const transporter = this.props.data.loading
+      ? { targets: [] }
+      : this.props.data.transporters[0];
+    return (
+      <div>
+        <p>Transporters</p>
+        {this.props.data.loading
+          ? <span>Loading...</span>
+          : this.props.data.transporters.length > 0
+            ? <div>
+                <OutputField
+                  alert={
+                    transporter.state === "Scanning" || this.state.transported
+                  }
+                >
+                  {this.state.transported
+                    ? "Transported"
+                    : `${transporter.state} ${transporter.state === "Charging"
+                        ? `- ${Math.round(transporter.charge * 100)}%`
+                        : ""} `}
+                </OutputField>
+                <OutputField>
+                  {transporter.requestedTarget}
+                </OutputField>
+                <OutputField>
+                  {transporter.destination}
+                </OutputField>
+                <InputField
+                  prompt="How many transporter targets?"
+                  onClick={this.targets.bind(this, transporter)}
+                >
+                  {transporter.targets.length}
+                </InputField>
+              </div>
+            : "No transporters"}
+      </div>
+    );
   }
 }
-
 
 const TRANSPORTERS_QUERY = gql`
-query GetTransporters($simulatorId: ID){
-  transporters(simulatorId: $simulatorId) {
-    id
-    state
-    charge
-    simulatorId
-    targets {
+  query GetTransporters($simulatorId: ID) {
+    transporters(simulatorId: $simulatorId) {
       id
+      state
+      charge
+      simulatorId
+      targets {
+        id
+      }
+      requestedTarget
+      destination
     }
-    requestedTarget
-    destination
   }
-}
 `;
 
-export default  graphql(TRANSPORTERS_QUERY, {
-    options: (ownProps) => ({ variables: { simulatorId: ownProps.simulator.id } }),
+export default graphql(TRANSPORTERS_QUERY, {
+  options: ownProps => ({ variables: { simulatorId: ownProps.simulator.id } })
 })(withApollo(TransporterCore));
