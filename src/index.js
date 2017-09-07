0170d9762e305ac145e594a074cafc81da9bda49
Alex Anderson <alexanderson1993@gmail.com>
2017-09-07 10:27:33 -0600

    Added a default value for the thorium global.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
+window.thorium = window.thorium || { sendMessage: () => {} };
 render(<App />, document.getElementById("root"));
