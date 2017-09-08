0730d086a65f12c953d5156999b610ef5d21c1e3
Alex Anderson <alexanderson1993@gmail.com>
2017-09-08 17:42:12 -0600

    Modifying the core layout jsut a bit for timeline.

--- src/components/core/layouts/default.js
+++ src/components/core/layouts/default.js
@@ -17,19 +17,24 @@ const exceptions = [
 ];
 export default props => {
   return (
-    <div className="core-default">
-      {Object.keys(Cores).filter(c => exceptions.indexOf(c) === -1).map(c => {
-        const Core = Cores[c];
-        const label = c.replace("Core", "");
-        return (
-          <div key={c} className={c}>
-            <p>
-              {label}
-            </p>
-            <Core {...props} />
-          </div>
-        );
-      })}
+    <div>
+      <div className="core-default">
+        {Object.keys(Cores).filter(c => exceptions.indexOf(c) === -1).map(c => {
+          const Core = Cores[c];
+          const label = c.replace("Core", "");
+          return (
+            <div key={c} className={c}>
+              <p>
+                {label}
+              </p>
+              <Core {...props} />
+            </div>
+          );
+        })}
+      </div>
+      <div className="timeline-core">
+        <Cores.TimelineCore {...props} />
+      </div>
     </div>
   );
 };
