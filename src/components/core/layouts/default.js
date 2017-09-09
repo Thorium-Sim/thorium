959cbcb93a78f29aba8ea180992d58668092c0ad
Alex Anderson <alexanderson1993@gmail.com>
2017-09-08 21:15:39 -0600

    Slide out timeline

--- src/components/core/layouts/default.js
+++ src/components/core/layouts/default.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Cores } from "../../views";
 import "./default.scss";
 
@@ -15,26 +15,39 @@ const exceptions = [
   "ProbeControlCore",
   "ProbeNetworkCore"
 ];
-export default props => {
-  return (
-    <div>
-      <div className="core-default">
-        {Object.keys(Cores).filter(c => exceptions.indexOf(c) === -1).map(c => {
-          const Core = Cores[c];
-          const label = c.replace("Core", "");
-          return (
-            <div key={c} className={c}>
-              <p>
-                {label}
-              </p>
-              <Core {...props} />
-            </div>
-          );
-        })}
+export default class CoreDefault extends Component {
+  state = { timelineOpen: false };
+  render() {
+    const { props, state } = this;
+    const { timelineOpen } = state;
+    return (
+      <div>
+        <div className="core-default">
+          {Object.keys(Cores)
+            .filter(c => exceptions.indexOf(c) === -1)
+            .map(c => {
+              const Core = Cores[c];
+              const label = c.replace("Core", "");
+              return (
+                <div key={c} className={c}>
+                  <p>
+                    {label}
+                  </p>
+                  <Core {...props} />
+                </div>
+              );
+            })}
+        </div>
+        <div className={`timeline-core ${timelineOpen ? "open" : ""}`}>
+          <div
+            className="timeline-label"
+            onClick={() => this.setState({ timelineOpen: !timelineOpen })}
+          >
+            Timeline
+          </div>
+          <Cores.TimelineCore {...props} />
+        </div>
       </div>
-      <div className="timeline-core">
-        <Cores.TimelineCore {...props} />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
