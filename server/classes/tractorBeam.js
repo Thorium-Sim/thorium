dd7e6507c1ddb91266ed3b28a10c888f7196db30
Alex Anderson <alexanderson1993@gmail.com>
2017-09-11 10:29:13 -0600

    Bug fixes for tractor beam and login name

--- server/classes/tractorBeam.js
+++ server/classes/tractorBeam.js
@@ -1,18 +1,18 @@
-import { System } from './generic';
+import { System } from "./generic";
 
 export default class TractorBeam extends System {
   constructor(params) {
     super(params);
-    this.class = 'TractorBeam';
-    this.type = 'TractorBeam';
-    this.name = params.name || 'Tractor Beam';
+    this.class = "TractorBeam";
+    this.type = "TractorBeam";
+    this.name = params.name || "Tractor Beam";
     this.state = params.state || false;
     this.target = params.target || false;
     this.strength = params.strength || 0.0;
     this.stress = params.stress || 0.15;
   }
   get stealthFactor() {
-    return (this.stress / 5 + .8);
+    return this.state ? this.stress / 5 + 0.8 : 0;
   }
   break(report) {
     this.state = false;
