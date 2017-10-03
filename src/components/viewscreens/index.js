b8d04d96cddf2d01cc0592dbf93414c5bc0253cf
Alex Anderson <alexanderson1993@gmail.com>
2017-10-03 11:35:15 -0600

    Added a ship views card

--- src/components/viewscreens/index.js
+++ src/components/viewscreens/index.js
@@ -15,9 +15,11 @@ export ReactorActivation from "./ReactorActivation";
 export PlanetaryScan from "./PlanetaryScan";
 export ShieldMonitoring from "./ShieldMonitoring";
 export CourseCalculation from "./CourseCalculation";
+export ShipView from "./ShipView";
 
 export InformationConfig from "./Information/config";
 export InternalSensorsConfig from "./InternalSensors/config";
 export ReactorActivationConfig from "./ReactorActivation/config";
 export PlanetaryScanConfig from "./PlanetaryScan/config";
 export CourseCalculationConfig from "./CourseCalculation/config";
+export ShipViewConfig from "./ShipView/config";
