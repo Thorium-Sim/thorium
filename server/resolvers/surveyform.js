c2a69e303e7c63fa60f9fa46c892c2a04512ecbe
Alex Anderson <alexanderson1993@gmail.com>
2018-03-05 11:59:49 -0700

    Add survey results button

--- server/resolvers/surveyform.js
+++ server/resolvers/surveyform.js
@@ -4,9 +4,14 @@ import { withFilter } from "graphql-subscriptions";
 
 export const SurveyFormQueries = {
   surveyform(root, { simulatorId, active }) {
-    let returnVal = App.surveyForms.filter(
-      s => (active ? s.active === true : true)
-    );
+    let returnVal = App.surveyForms.filter(s => {
+      if (active === true) {
+        return s.active === true;
+      } else if (active === false) {
+        return !s.active;
+      }
+      return true;
+    });
     if (simulatorId) {
       return returnVal.filter(i => i.simulatorId === simulatorId);
     }
@@ -29,6 +34,9 @@ export const SurveyFormMutations = {
   },
   surveyFormResponse(root, args, context) {
     App.handleEvent(args, "surveyFormResponse", context);
+  },
+  endSurvey(root, args, context) {
+    App.handleEvent(args, "endSurvey", context);
   }
 };
 
@@ -37,11 +45,25 @@ export const SurveyFormSubscriptions = {
     resolve(rootValue, { simulatorId, active }) {
       if (simulatorId) {
         return rootValue
-          .filter(s => (active ? s.active === true : true))
+          .filter(s => {
+            if (active === true) {
+              return s.active === true;
+            } else if (active === false) {
+              return !s.active;
+            }
+            return true;
+          })
           .filter(s => s.simulatorId === simulatorId);
       }
       return rootValue
-        .filter(s => (active ? s.active === true : true))
+        .filter(s => {
+          if (active === true) {
+            return s.active === true;
+          } else if (active === false) {
+            return !s.active;
+          }
+          return true;
+        })
         .filter(s => s.simulatorId === null);
     },
     subscribe: withFilter(
