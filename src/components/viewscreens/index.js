db7c529d7dbdc91b2ad6db12cac1902bfb467dc8
Alex Anderson <alexanderson1993@gmail.com>
2018-03-19 15:58:26 -0600

    Add Intruder monitoring card

--- src/components/viewscreens/index.js
+++ src/components/viewscreens/index.js
@@ -27,6 +27,7 @@ export { default as DamageMonitoring } from "./DamageMonitoring";
 export { default as Downloading } from "./Downloading";
 export { default as YellowAlert } from "./YellowAlert";
 export { default as CollisionAlert } from "./CollisionAlert";
+export { default as IntruderMonitoring } from "./IntruderMonitoring";
 
 export { default as InformationConfig } from "./Information/config";
 export { default as InternalSensorsConfig } from "./InternalSensors/config";
