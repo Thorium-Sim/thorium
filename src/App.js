c1fc11197f37738331583a6c7a7064804ba99590
Alex Anderson <alexanderson1993@gmail.com>
2018-03-19 08:30:34 -0600

    Added an easter egg.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { ApolloProvider } from "react-apollo";
 import client from "./helpers/graphqlClient";
 import App from "./containers/App";
 import ErrorBoundary from "./helpers/errorBoundary";
-
+import EasterEgg from "./helpers/easter-egg";
 import "./app.css";
 
 const ApolloApp = () => (
   <ApolloProvider client={client}>
     <ErrorBoundary>
       <App />
+      <EasterEgg />
     </ErrorBoundary>
   </ApolloProvider>
 );
---
+++ src/helpers/easter-egg.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Konami from "react-konami";
+const synth = window.speechSynthesis;
+
+/** A tribute to my dad **/
+const song = `The thunder god a riding went upon his favorite filly.
+I’m Thor he cried, His horse replied “You forgot your thaddle thilly”`;
+
+export default () => {
+  return (
+    <Konami
+      easterEgg={() => {
+        synth.cancel();
+        synth.speak(new SpeechSynthesisUtterance(song));
+      }}
+    />
+  );
+};
