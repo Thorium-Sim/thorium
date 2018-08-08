a319d371f1e6e66983c744e465592cfdd3a15a7a
Alex Anderson <alexanderson1993@gmail.com>
2018-08-08 17:36:09 -0600

    Remove unnecessary code comments.

--- src/components/viewscreens/PlanetaryScan/index.js
+++ src/components/viewscreens/PlanetaryScan/index.js
@@ -16,7 +16,6 @@ class RenderSphere extends Component {
     });
   };
   render() {
-    //const cameraPosition = new THREE.Vector3(0, 2, 4);
     const { text } = this.props;
     const { dimensions } = this.state;
     return (
@@ -37,59 +36,6 @@ class RenderSphere extends Component {
           )}
         </Measure>
         <div className="text-box">{text}</div>
-        {/*<React3
-          alpha
-          mainCamera="camera"
-          width={window.innerHeight}
-          height={window.innerHeight}
-          onAnimate={this._onAnimate}
-        >
-          <scene>
-            <perspectiveCamera
-              name="camera"
-              fov={45}
-              aspect={1}
-              near={0.1}
-              far={1000}
-              lookAt={new THREE.Vector3(0, 0, 0)}
-              position={cameraPosition}
-            />
-            <directionalLight
-              color={0xffffff}
-              intensity={1}
-              lookAt={new THREE.Vector3(0, 0, 0)}
-              position={new THREE.Vector3(0, 0, 1)}
-            />
-            <directionalLight
-              color={0xffffff}
-              intensity={1}
-              lookAt={new THREE.Vector3(0, 0, 0)}
-              position={new THREE.Vector3(0, 0, -1)}
-            />
-            {src &&
-              <mesh rotation={new THREE.Euler(0, this.state.rotation, 0)}>
-                <sphereGeometry
-                  radius={1}
-                  widthSegments={32}
-                  heightSegments={32}
-                />
-                <meshPhongMaterial shininess={0} wireframe={wireframe}>
-                  {!wireframe && <texture url={src} />}
-                </meshPhongMaterial>
-              </mesh>}
-            {clouds &&
-              <mesh rotation={new THREE.Euler(0, this.state.rotation / 5, 0)}>
-                <sphereGeometry
-                  radius={1.05}
-                  widthSegments={16}
-                  heightSegments={16}
-                />
-                <meshPhongMaterial transparent={true}>
-                  <texture url={clouds} />
-                </meshPhongMaterial>
-              </mesh>}
-          </scene>
-            </React3>*/}
       </div>
     );
   }
