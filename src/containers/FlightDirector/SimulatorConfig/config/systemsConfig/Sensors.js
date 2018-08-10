920c7ac2c659eafd8697e41d139f98b8de137d8a
Alex Anderson <alexanderson1993@gmail.com>
2018-08-10 07:18:29 -0600

    Pre-configure sensors auto-targeting.

--- src/containers/FlightDirector/SimulatorConfig/config/systemsConfig/Sensors.js
+++ src/containers/FlightDirector/SimulatorConfig/config/systemsConfig/Sensors.js
@@ -17,33 +17,56 @@ const Sensors = props => {
       <Query query={SENSORS_QUERY} variables={{ id }}>
         {({ data, loading }) => {
           if (loading) return null;
-          const { history } = data.sensor;
+          const { history, autoTarget } = data.sensor;
           return (
-            <label>
-              <Mutation
-                mutation={gql`
-                  mutation SensorsHistory($id: ID!, $history: Boolean!) {
-                    setSensorsHistory(id: $id, history: $history)
-                  }
-                `}
-                refetchQueries={[{ query: SENSORS_QUERY, variables: { id } }]}
-              >
-                {action => (
-                  <input
-                    type="checkbox"
-                    checked={history}
-                    onChange={() =>
-                      action({ variables: { id, history: !history } })
+            <Fragment>
+              <label>
+                <Mutation
+                  mutation={gql`
+                    mutation SensorsHistory($id: ID!, $history: Boolean!) {
+                      setSensorsHistory(id: $id, history: $history)
                     }
-                  />
-                )}
-              </Mutation>
-              Scanning History
-            </label>
+                  `}
+                  refetchQueries={[{ query: SENSORS_QUERY, variables: { id } }]}
+                >
+                  {action => (
+                    <input
+                      type="checkbox"
+                      checked={history}
+                      onChange={() =>
+                        action({ variables: { id, history: !history } })
+                      }
+                    />
+                  )}
+                </Mutation>
+                Scanning History
+              </label>
+              <label>
+                Auto-add to targeting{" "}
+                <Mutation
+                  mutation={gql`
+                    mutation SensorsAutoTarget($id: ID!, $target: Boolean!) {
+                      toggleSensorsAutoTarget(id: $id, target: $target)
+                    }
+                  `}
+                >
+                  {action => (
+                    <input
+                      type="checkbox"
+                      checked={autoTarget}
+                      onClick={e =>
+                        action({ variables: { id, target: e.target.checked } })
+                      }
+                    />
+                  )}
+                </Mutation>
+              </label>
+            </Fragment>
           );
         }}
       </Query>
     </GenericSystemConfig>
   );
 };
+
 export default Sensors;
