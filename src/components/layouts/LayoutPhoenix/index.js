65a7954ef607f74b397ad1028194ff0e80a087c1
Alex Anderson <alexanderson1993@gmail.com>
2018-08-11 21:33:01 -0600

    Initial work on the Phoenix frame.

---
+++ src/components/layouts/LayoutPhoenix/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CardFrame from "./cardFrame";
+const LayoutPhoenix = props => {
+  return (
+    <div className="phoenix-frame">
+      <CardFrame {...props} />
+    </div>
+  );
+};
+export default LayoutPhoenix;
+import "./style.scss";
