a8e82ccc6ef0b761999663f0ed9be68b746749d9
Alex Anderson <alexanderson1993@gmail.com>
2018-09-29 13:34:22 -0600

    Start sketching out how tasks will work.

---
+++ server/src/classes/task.js
@@ -0,0 +1,26 @@
+import uuid from "uuid";
+
+class TaskTemplate {
+  constructor(params = {}) {
+    this.id = params.id || uuid.v4();
+  }
+}
+class Task {
+  constructor(params = {}) {
+    this.id = params.id || uuid.v4();
+    this.simulatorId = params.simulatorId || "";
+    this.systemId = params.systemId || "";
+    this.deck = params.deck || "";
+    this.room = params.room || "";
+
+    // The static object which defines the values and
+    // methods for the task
+    this.generator = params.generator || "Generic";
+
+    // The template
+    this.taskTemplate = params.taskTemplate || "";
+
+    // Task parameters
+    this.verified = params.verified || false;
+  }
+}
