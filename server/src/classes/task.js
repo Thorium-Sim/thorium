a1b4f1b7c3ecc0f1055cefcac9ef946688ece81c
Alex Anderson <alexanderson1993@gmail.com>
2018-10-06 15:08:30 -0600

    More log removal 😤

--- server/src/classes/task.js
+++ server/src/classes/task.js
@@ -5,7 +5,7 @@ import * as classes from "./";
 const taskDefinitions = Object.values(classes)
   .reduce((prev, c) => prev.concat(c.tasks), [])
   .filter(Boolean);
-console.log(taskDefinitions);
+
 export default class Task {
   static tasks = [
     // Simple, generic task definition.
