c1a1cb8abb489c14377b66bd51b894f949d9cbb5
Alex Anderson <alexanderson1993@gmail.com>
2018-10-12 08:31:22 -0600

    Fix an issue with exporting keyboards. Closes #1427

--- server/src/imports/missions/export/playSound.js
+++ server/src/imports/missions/export/playSound.js
@@ -3,6 +3,8 @@ import addAsset from "../../addAsset";
 export default function buildExport(zip, i, type) {
   if (i.event === "playSound") {
     const args = JSON.parse(i.args);
+    console.log({ i, args });
+    if (!args.sound) return;
     const asset = args.sound.asset;
     if (asset) {
       addAsset(asset, zip, type);
