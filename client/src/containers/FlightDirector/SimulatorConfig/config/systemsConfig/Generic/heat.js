f23ed061c1116f1b7f69fd52f049b1bf3f7f35c5
Alex Anderson <alexanderson1993@gmail.com>
2018-11-07 09:01:56 -0700

    Add ability to change heat rate in the systems config. Closes #1468

---
+++ client/src/containers/FlightDirector/SimulatorConfig/config/systemsConfig/Generic/heat.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FormGroup, Input, Label, Button } from "reactstrap";
+import { Mutation } from "react-apollo";
+import FontAwesome from "react-fontawesome";
+import { GENERIC_QUERY } from "./index";
+import gql from "graphql-tag";
+
+const Power = ({ id, simulatorId, heatRate }) => {
+  return (
+    <FormGroup>
+      <Label>Heat Rate (lower is slower)</Label>
+      <Mutation
+        mutation={gql`
+          mutation SetHeatRate($id: ID!, $rate: Float) {
+            setHeatRate(id: $id, rate: $rate)
+          }
+        `}
+        refetchQueries={[
+          { query: GENERIC_QUERY, variables: { id, simulatorId } }
+        ]}
+      >
+        {action => (
+          <Input
+            type="number"
+            min={0}
+            defaultValue={heatRate}
+            onChange={e => action({ variables: { id, rate: e.target.value } })}
+          />
+        )}
+      </Mutation>
+    </FormGroup>
+  );
+};
+export default Power;
