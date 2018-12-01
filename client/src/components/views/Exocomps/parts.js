37a69759c8430ac377e9d794cd3bdbe21f5b357d
Alex Anderson <alexanderson1993@gmail.com>
2018-12-01 13:26:15 -0700

    Add a mock for require.context

--- client/src/components/views/Exocomps/parts.js
+++ client/src/components/views/Exocomps/parts.js
@@ -1,3 +1,42 @@
+if (typeof require.context === "undefined") {
+  const fs = require("fs");
+  const path = require("path");
+
+  require.context = (
+    base = ".",
+    scanSubDirectories = false,
+    regularExpression = /\.js$/
+  ) => {
+    const files = {};
+
+    function readDirectory(directory) {
+      fs.readdirSync(directory).forEach(file => {
+        const fullPath = path.resolve(directory, file);
+
+        if (fs.statSync(fullPath).isDirectory()) {
+          if (scanSubDirectories) readDirectory(fullPath);
+
+          return;
+        }
+
+        if (!regularExpression.test(fullPath)) return;
+
+        files[fullPath] = true;
+      });
+    }
+
+    readDirectory(path.resolve(__dirname, base));
+
+    function Module(file) {
+      return require(file);
+    }
+
+    Module.keys = () => Object.keys(files);
+
+    return Module;
+  };
+}
+
 function importAll(r) {
   return r
     .keys()
