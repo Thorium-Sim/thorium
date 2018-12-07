738a0aceb156b7e42f1c6adf8d7db6ce343878d0
Alex Anderson <alexanderson1993@gmail.com>
2018-12-06 17:47:28 -0700

    Add a room search core and added room search/select to the internal comm core. Closes #1611

---
+++ client/src/components/views/RoomSearch/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import RoomSearch from "../CommInternal/roomSearch";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const QUERY = gql`
+  query Decks($simulatorId: ID!) {
+    decks(simulatorId: $simulatorId) {
+      id
+      number
+      rooms {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const RoomSearchData = props => {
+  return (
+    <Query query={QUERY} variables={{ simulatorId: props.simulator.id }}>
+      {({ loading, data: { decks } }) =>
+        loading ? null : <RoomSearch decks={decks} />
+      }
+    </Query>
+  );
+};
+
+export default RoomSearchData;
