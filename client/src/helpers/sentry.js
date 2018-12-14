96e2712aa9cd5b221aba62027f9c0cf83bc52b36
Alex Anderson <alexanderson1993@gmail.com>
2018-12-14 13:43:40 -0700

    Final fixes. Closes #1604

--- client/src/helpers/sentry.js
+++ client/src/helpers/sentry.js
@@ -1,5 +1,7 @@
 import * as Sentry from "@sentry/browser";
 
-// Sentry.init({
-//   dsn: "https://c71a50635f9b42f4a70880cce6a7ff0e@sentry.io/1323903"
-// });
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://c71a50635f9b42f4a70880cce6a7ff0e@sentry.io/1323903"
+  });
+}
