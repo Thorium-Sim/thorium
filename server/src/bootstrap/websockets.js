ee8ec9006d1bb5b81008decc681e241ce1fd4a41
Alex Anderson <alexanderson1993@gmail.com>
2019-01-31 14:04:02 -0700

    Closes Thorium if another version is running. Closes #1619

--- server/src/bootstrap/websockets.js
+++ server/src/bootstrap/websockets.js
@@ -1,13 +1,23 @@
 import { createServer } from "http";
 import { SubscriptionServer } from "subscriptions-transport-ws";
 import { execute, subscribe } from "graphql";
+import chalk from "chalk";
 import schema from "../data";
 
 export default (server, PORT) => {
   const ws = createServer(server);
+  ws.on("error", err => {
+    if (err.code === "EADDRINUSE") {
+      console.log(
+        chalk.redBright(
+          "There is already a version of Thorium running on this computer. Shutting down..."
+        )
+      );
+      process.exit(0);
+    }
+  });
   ws.listen(PORT, () => {
     // Set up the WebSocket for handling GraphQL subscriptions
-
     new SubscriptionServer( // eslint-disable-line
       {
         execute,
