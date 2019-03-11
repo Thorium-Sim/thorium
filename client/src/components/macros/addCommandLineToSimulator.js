f08b88adff30591ffac5bbaf7a64ad4a173fbcc6
Alex Anderson <alexanderson1993@gmail.com>
2019-03-11 11:07:38 -0600

    Fixes command line config option

--- client/src/components/macros/addCommandLineToSimulator.js
+++ client/src/components/macros/addCommandLineToSimulator.js
@@ -4,41 +4,42 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag.macro";
 
 export default ({ updateArgs, args, client }) => {
+  console.log(args);
   return (
-    <FormGroup className="macro-addCommandLine">
-      <Label>Command Line</Label>
-      <Input
-        type="select"
-        value={args.commandLine || "nothing"}
-        onChange={e => updateArgs("commandLine", e.target.value)}
-      >
-        <option value="nothing" disabled>
-          Select a command line.
-        </option>
-        <Query
-          query={gql`
-            query CommandLines {
-              commandLine {
-                id
-                name
-                commands {
-                  name
-                }
-              }
+    <Query
+      query={gql`
+        query CommandLines {
+          commandLine {
+            id
+            name
+            commands {
+              name
             }
-          `}
-        >
-          {({ loading, data: { commandLine } }) =>
-            loading
-              ? null
-              : commandLine.map(c => (
-                  <option key={c.id} value={c.id}>
-                    {c.name}
-                  </option>
-                ))
           }
-        </Query>
-      </Input>
-    </FormGroup>
+        }
+      `}
+    >
+      {({ loading, data: { commandLine } }) =>
+        loading ? null : (
+          <FormGroup className="macro-addCommandLine">
+            <Label>Command Line</Label>
+            <Input
+              type="select"
+              value={args.commandLine || "nothing"}
+              onChange={e => updateArgs("commandLine", e.target.value)}
+            >
+              <option value="nothing" disabled>
+                Select a command line.
+              </option>
+              {commandLine.map(c => (
+                <option key={c.id} value={c.id}>
+                  {c.name}
+                </option>
+              ))}
+            </Input>
+          </FormGroup>
+        )
+      }
+    </Query>
   );
 };
