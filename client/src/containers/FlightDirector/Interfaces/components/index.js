339d1300b74d303efa93358d1c6e6d4bdd633c73
Alex Anderson <alexanderson1993@gmail.com>
2019-04-12 15:17:33 -0600

    Add the toggle component for interfaces.

--- client/src/containers/FlightDirector/Interfaces/components/index.js
+++ client/src/containers/FlightDirector/Interfaces/components/index.js
@@ -1,3 +1,4 @@
 // export { default as switch } from "./switch";
 export { default as screen } from "./screen";
 export { default as button } from "./button";
+export { default as toggle } from "./toggle";
