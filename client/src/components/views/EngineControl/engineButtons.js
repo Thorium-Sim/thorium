1bff72baa695a9a77c20060050f6f1aa1e47f7af
Alex Anderson <alexanderson1993@gmail.com>
2019-05-02 10:16:14 -0600

    Fixes issues with engine buttons. Closes #2121

---
+++ client/src/components/views/EngineControl/engineButtons.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+export default function EngineButtons({ engine, locked, engines, setSpeed }) {
+  return engine.speeds.map((speed, speedIndex) => {
+    let speedWord = speed;
+    if (typeof speed === "object") {
+      speedWord = speed.text;
+    }
+    const powerIndex = engine.power.powerLevels.reduce((acc, p, i) => {
+      if (engine.power.power >= p) return i;
+      return acc;
+    }, -1);
+    return (
+      <Button
+        disabled={locked || engine.damage.damaged || speedIndex > powerIndex}
+        key={`speed-${speedIndex}`}
+        color="primary"
+        block
+        className="speedBtn"
+        onClick={() => {
+          setSpeed(engine, speedIndex, engines, 0);
+        }}
+      >
+        {speedWord}
+      </Button>
+    );
+  });
+}
