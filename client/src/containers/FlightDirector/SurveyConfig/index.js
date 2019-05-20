0726d38b377a9fc7009d898e5970f62b5f3e97b7
Alex Anderson <alexanderson1993@gmail.com>
2019-05-20 08:12:14 -0600

    Fix up another component.

--- client/src/containers/FlightDirector/SurveyConfig/index.js
+++ client/src/containers/FlightDirector/SurveyConfig/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Container,
   Row,
@@ -8,8 +8,9 @@ import {
   Button
 } from "reactstrap";
 import gql from "graphql-tag.macro";
-import { graphql, withApollo } from "react-apollo";
-import SubscriptionHelper from "helpers/subscriptionHelper";
+import { useQuery } from "@apollo/react-hooks";
+import { useSubscribeToMore } from "helpers/hooks/useQueryAndSubscribe";
+import { withApollo } from "react-apollo";
 import Form from "./form";
 import "./style.scss";
 
@@ -35,10 +36,32 @@ const SUB = gql`
     }
   }
 `;
+const QUERY = gql`
+  query SurveyForms {
+    surveyform(simulatorId: null, active: false) {
+      id
+      form {
+        id
+        type
+        title
+        value
+        options {
+          id
+          label
+        }
+        description
+        min
+        max
+      }
+      title
+      simulatorId
+    }
+  }
+`;
 
-class Surveys extends Component {
-  state = {};
-  createForm = () => {
+const Surveys = ({ client }) => {
+  const [selectedForm, setSelectedForm] = React.useState(null);
+  const createForm = () => {
     const name = window.prompt("What is the title of the form?");
     if (!name) return;
     const mutation = gql`
@@ -49,43 +72,40 @@ class Surveys extends Component {
     const variables = {
       name
     };
-    this.props.client
+    client
       .mutate({
         mutation,
         variables
       })
       .then(({ data }) => {
-        this.setState({ selectedForm: data && data.createSurveyForm });
+        setSelectedForm(data && data.createSurveyForm);
       });
   };
-  removeForm = () => {
+  const removeForm = () => {
     if (!window.confirm("Are you sure you want to delete this survey?")) return;
-    const { selectedForm: id } = this.state;
     const mutation = gql`
       mutation RemoveForm($id: ID!) {
         removeSurveyForm(id: $id)
       }
     `;
     const variables = {
-      id
+      id: selectedForm
     };
-    this.setState({
-      selectedForm: null
-    });
-    this.props.client.mutate({
+    setSelectedForm(null);
+
+    client.mutate({
       mutation,
       variables
     });
   };
-  saveForm = form => {
-    const { selectedForm: id } = this.state;
+  const saveForm = form => {
     const mutation = gql`
       mutation SaveForm($id: ID!, $form: [FormFieldsInput]!) {
         updateSurveyForm(id: $id, form: $form)
       }
     `;
     const variables = {
-      id,
+      id: selectedForm,
       form: form.map(
         ({
           id: formId,
@@ -110,102 +130,70 @@ class Surveys extends Component {
         }
       )
     };
-    this.props.client.mutate({
+    client.mutate({
       mutation,
       variables
     });
   };
-  render() {
-    const {
-      data: { loading, surveyform }
-    } = this.props;
-    const { selectedForm } = this.state;
-    if (loading || !surveyform) return null;
-    return (
-      <Container fluid className="survey-forms">
-        <SubscriptionHelper
-          subscribe={() =>
-            this.props.data.subscribeToMore({
-              document: SUB,
-              updateQuery: (previousResult, { subscriptionData }) => {
-                return Object.assign({}, previousResult, {
-                  surveyform: subscriptionData.data.surveyformUpdate
-                });
-              }
-            })
-          }
-        />
-        <h4>Survey Config </h4>
-
-        <Row>
-          <Col sm={3}>
-            <div
-              style={{
-                maxHeight: "60vh",
-                overflowY: "scroll"
-              }}
-            >
-              <ListGroup>
-                {surveyform.map(s => (
-                  <ListGroupItem
-                    key={s.id}
-                    active={selectedForm === s.id}
-                    tag="button"
-                    action
-                    onClick={() => this.setState({ selectedForm: s.id })}
-                  >
-                    {s.title}
-                  </ListGroupItem>
-                ))}
-              </ListGroup>
-            </div>
-            <Button color="success" block onClick={this.createForm}>
-              Create Form
-            </Button>
-            {selectedForm && (
-              <Button color="danger" block onClick={this.removeForm}>
-                Remove Form
-              </Button>
-            )}
-          </Col>
-          <Col sm={9}>
-            {selectedForm && (
-              <Form
-                saveForm={this.saveForm}
-                form={
-                  surveyform.find(f => f.id === selectedForm) &&
-                  surveyform.find(f => f.id === selectedForm).form
-                }
-              />
-            )}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
-
-const QUERY = gql`
-  query SurveyForms {
-    surveyform(simulatorId: null, active: false) {
-      id
-      form {
-        id
-        type
-        title
-        value
-        options {
-          id
-          label
-        }
-        description
-        min
-        max
-      }
-      title
-      simulatorId
+  const { loading, data, subscribeToMore } = useQuery(QUERY);
+  useSubscribeToMore(subscribeToMore, SUB, {
+    updateQuery: (previousResult, { subscriptionData }) => {
+      return Object.assign({}, previousResult, {
+        surveyform: subscriptionData.data.surveyformUpdate
+      });
     }
-  }
-`;
+  });
+  if (loading || !data) return null;
+  const { surveyform } = data;
+  return (
+    <Container fluid className="survey-forms">
+      <h4>Survey Config </h4>
 
-export default graphql(QUERY)(withApollo(Surveys));
+      <Row>
+        <Col sm={3}>
+          <div
+            style={{
+              maxHeight: "60vh",
+              overflowY: "scroll"
+            }}
+          >
+            <ListGroup>
+              {surveyform.map(s => (
+                <ListGroupItem
+                  key={s.id}
+                  active={selectedForm === s.id}
+                  tag="button"
+                  action
+                  onClick={() => setSelectedForm(s.id)}
+                >
+                  {s.title}
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </div>
+          <Button color="success" block onClick={createForm}>
+            Create Form
+          </Button>
+          {selectedForm && (
+            <Button color="danger" block onClick={removeForm}>
+              Remove Form
+            </Button>
+          )}
+        </Col>
+        <Col sm={9}>
+          {selectedForm && (
+            <Form
+              saveForm={saveForm}
+              form={
+                surveyform.find(f => f.id === selectedForm) &&
+                surveyform.find(f => f.id === selectedForm).form
+              }
+            />
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default withApollo(Surveys);
