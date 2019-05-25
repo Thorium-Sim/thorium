92ce4799ceda31d8ee7365dc2add892849b3eb87
Alex Anderson <alexanderson1993@gmail.com>
2019-05-25 13:22:31 -0600

    feat(Interfaces): Adds interfaces as an option for set configs. Closes #2163

--- server/src/helpers/stationResolver.js
+++ server/src/helpers/stationResolver.js
@@ -26,8 +26,31 @@ export const StationResolver = rootValue => {
       ]
     };
   }
+
   const simulator = App.simulators.find(s => s.id === rootValue.simulatorId);
   if (simulator) {
+    if (
+      rootValue.station &&
+      rootValue.station.match(/interface-id:.{8}-.{4}-.{4}-.{4}-.{12}/gi)
+    ) {
+      const interfaces = simulator.interfaces.map(i =>
+        App.interfaces.find(({ id }) => i === id)
+      );
+      const iface = interfaces.find(
+        i => i.templateId === rootValue.station.replace("interface-id:", "")
+      );
+      if (iface) {
+        return {
+          name: `interface-id:${iface.id}`,
+          cards: [
+            {
+              name: "Interface",
+              component: `interface-id:${iface.id}`
+            }
+          ]
+        };
+      }
+    }
     const station = simulator.stations.find(s => s.name === rootValue.station);
     if (station) return station;
     // Fallback for Viewscreen, Blackout, and Mobile
