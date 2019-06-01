de655f9b59b337acfd314eb1dcc232fa9ea3d7db
Alex Anderson <alexanderson1993@gmail.com>
2019-05-31 21:47:56 -0600

    fix(Surveys): Make it possible to reorder survey form items. Closes #2247

--- client/src/containers/FlightDirector/SurveyConfig/form-container.js
+++ client/src/containers/FlightDirector/SurveyConfig/form-container.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Input, Button } from "reactstrap";
 import * as Components from "./components";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 const style = {
+  display: "flex",
   border: "solid 1px #333",
   minHeight: "50px",
   backgroundColor: "#030303",
@@ -16,28 +18,45 @@ export default class FormContainer extends React.Component {
       title,
       description,
       id,
+      i,
+      length,
       updateForm,
-      removeField
+      removeField,
+      reorder
     } = this.props;
     const Comp = Components[type];
     if (!Comp) return null;
     return (
       <div style={style}>
-        <Input
-          value={title}
-          placeholder="Title goes here"
-          onChange={e => updateForm(id, "title", e.target.value)}
-        />
-        <Input
-          type="textarea"
-          value={description}
-          placeholder="Description goes here"
-          onChange={e => updateForm(id, "description", e.target.value)}
-        />
-        <Comp {...this.props} disabled />
-        <Button color={"danger"} outline onClick={() => removeField(id)}>
-          Remove Field
-        </Button>
+        <div style={{ flex: 1 }}>
+          <Input
+            value={title}
+            placeholder="Title goes here"
+            onChange={e => updateForm(id, "title", e.target.value)}
+          />
+          <Input
+            type="textarea"
+            value={description}
+            placeholder="Description goes here"
+            onChange={e => updateForm(id, "description", e.target.value)}
+          />
+          <Comp {...this.props} disabled />
+          <Button color={"danger"} outline onClick={() => removeField(id)}>
+            Remove Field
+          </Button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+            padding: "0 5px"
+          }}
+        >
+          {i !== 0 && <FaArrowUp onClick={() => reorder(-1)} />}
+          {i + 1 !== length && <FaArrowDown onClick={() => reorder(1)} />}
+        </div>
       </div>
     );
   }
