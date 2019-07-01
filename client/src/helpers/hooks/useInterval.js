913022fa223b2dc069f2f4dacc2cad6c5c28be23
Alex Anderson <alexanderson1993@gmail.com>
2019-07-01 07:43:39 -0600

    cleanup

--- client/src/helpers/hooks/useInterval.js
+++ client/src/helpers/hooks/useInterval.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function useInterval(callback, delay) {
   const savedCallback = useRef();
