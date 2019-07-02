c9354ee8b824bdf4539b55332ff68026c8e3332d
Alex Anderson <alexanderson1993@gmail.com>
2019-07-02 07:19:39 -0600

    fix(Notifications): Add an option to disable command line notifications and core feed items. Closes #2365

--- client/src/components/views/CoreFeed/config.js
+++ client/src/components/views/CoreFeed/config.js
@@ -14,6 +14,7 @@ import { titleCase } from "change-case";
 const components = [
   "AlertConditionCore",
   "CargoCore",
+  "CommandLineCore",
   "CommShortRangeCore",
   "DamageReportsCore",
   "DamageTeamsCore",
