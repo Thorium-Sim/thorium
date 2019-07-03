fecad55448bea8753f509e3bf6a73d91825e9bc9
Alex Anderson <alexanderson1993@gmail.com>
2019-07-03 17:48:15 -0600

    fix(Errors): Decreases the number of errors being reported, which keeps me in the limit for my error tracker. Closes #2400

--- client/src/helpers/sentry.js
+++ client/src/helpers/sentry.js
@@ -1,14 +1,15 @@
 import * as Sentry from "@sentry/browser";
 import { version } from "../../package.json";
 
+const invalidMessages = ["ResizeObserver loop limit exceeded"];
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
     release: `react@${version}`,
     dsn: "https://c71a50635f9b42f4a70880cce6a7ff0e@sentry.io/1323903",
     beforeSend(event) {
       if (
-        event.exception.mechanism.data.message ===
-        "ResizeObserver loop limit exceeded"
+        event.exception.mechanism.data &&
+        invalidMessages.includes(event.exception.mechanism.data.message)
       )
         return null;
       return event;
