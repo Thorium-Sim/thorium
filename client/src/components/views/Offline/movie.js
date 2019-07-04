230d11c50ce5100a0611a60b27cd6798802d4da5
Alex Anderson <alexanderson1993@gmail.com>
2019-07-03 18:05:32 -0600

    fix(Actions): Makes it so setting a movie on a station properly works from the actions macro. Closes #2397

--- client/src/components/views/Offline/movie.js
+++ client/src/components/views/Offline/movie.js
@@ -1,53 +1,46 @@
-import React, { PureComponent } from "react";
-import { Asset } from "helpers/assets";
+import React from "react";
 
-class Movie extends PureComponent {
-  state = { error: false };
-  render() {
-    const { movie } = this.props;
-    return (
-      <div
-        style={{
-          height: "100vh",
-          width: "100vw",
-          position: "fixed",
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          zIndex: 100000000
-        }}
-      >
-        <Asset asset={movie}>
-          {({ src }) =>
-            this.state.error ? (
-              <img
-                src={src}
-                style={{
-                  height: "100%",
-                  width: "100%",
-                  objectFit: "contain"
-                }}
-                alt="movie"
-              />
-            ) : (
-              <video
-                onError={() => this.setState({ error: true })}
-                muted
-                src={src}
-                style={{
-                  height: "100%",
-                  width: "100%"
-                }}
-                autoPlay
-                loop
-                controls={false}
-              />
-            )
-          }
-        </Asset>
-      </div>
-    );
-  }
-}
+const Movie = ({ movie }) => {
+  const [error, setError] = React.useState(false);
+  React.useEffect(() => setError(false), [movie]);
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        position: "fixed",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        zIndex: 100000000
+      }}
+    >
+      {error ? (
+        <img
+          src={`/assets${movie}`}
+          style={{
+            height: "100%",
+            width: "100%",
+            objectFit: "contain"
+          }}
+          alt="movie"
+        />
+      ) : (
+        <video
+          onError={() => setError(true)}
+          muted
+          src={`/assets${movie}`}
+          style={{
+            height: "100%",
+            width: "100%"
+          }}
+          autoPlay
+          loop
+          controls={false}
+        />
+      )}
+    </div>
+  );
+};
 export default Movie;
