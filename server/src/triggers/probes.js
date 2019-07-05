c971012e27343f038ebf245b9f5134638f96d52b
Alex Anderson <alexanderson1993@gmail.com>
2019-07-05 17:35:38 -0600

    feat(Triggers): Add a trigger for probe processed data. Closes #2358

--- server/src/triggers/probes.js
+++ server/src/triggers/probes.js
@@ -5,3 +5,9 @@ export function launchProbe({ id }) {
   if (!system) return {};
   return { simulatorId: system.simulatorId };
 }
+export function probeProcessedData({ id, simulatorId }) {
+  if (simulatorId) return { simulatorId };
+  const system = App.systems.find(s => s.id === id);
+  if (!system) return {};
+  return { simulatorId: system.simulatorId };
+}
