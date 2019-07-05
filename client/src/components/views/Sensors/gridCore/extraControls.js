b5ccda962560b7cd8415c13bfd2e2cfe6500176b
Alex Anderson <alexanderson1993@gmail.com>
2019-07-05 17:13:52 -0600

    feat(Sensors): Planet, border, and ping settings are now saved and retained.

--- client/src/components/views/Sensors/gridCore/extraControls.js
+++ client/src/components/views/Sensors/gridCore/extraControls.js
@@ -6,6 +6,7 @@ import tinycolor from "tinycolor2";
 import gql from "graphql-tag.macro";
 import { TypingField } from "../../../generic/core";
 import Nudge from "./nudge";
+import useLocalStorage from "helpers/hooks/useLocalStorage";
 
 function useOnClickOutside(ref, handler) {
   useEffect(
@@ -95,250 +96,270 @@ export const ColorPicker = ({ color, onChangeComplete }) => {
   );
 };
 
-class ExtraControls extends Component {
-  state = {
-    planetSize: 1,
-    planetColor: "#663399",
-    planetLabel: "",
-    borderColor: "#663399",
-    borderSize: 3,
-    borderLabel: "",
-    pingColor: "#663399",
-    pingSize: 1
-  };
-  autoTarget = e => {
+const ExtraControls = ({
+  sensors,
+  askForSpeed,
+  updateAskForSpeed,
+  client,
+  speed,
+  dragStart
+}) => {
+  const [planetSize, setPlanetSize] = useLocalStorage(
+    "core_sensors_planetSize",
+    1
+  );
+  const [planetColor, setPlanetColor] = useLocalStorage(
+    "core_sensors_planetColor",
+    "#663399"
+  );
+  const [planetLabel, setPlanetLabel] = useLocalStorage(
+    "core_sensors_planetLabel",
+    ""
+  );
+  const [borderColor, setBorderColor] = useLocalStorage(
+    "core_sensors_borderColor",
+    "#663399"
+  );
+  const [borderSize, setBorderSize] = useLocalStorage(
+    "core_sensors_borderSize",
+    3
+  );
+  const [borderLabel, setBorderLabel] = useLocalStorage(
+    "core_sensors_borderLabel",
+    ""
+  );
+  const [pingColor, setPingColor] = useLocalStorage(
+    "core_sensors_pingColor",
+    "#663399"
+  );
+  const [pingSize, setPingSize] = useLocalStorage("core_sensors_pingSize", 1);
+  const autoTarget = e => {
     const mutation = gql`
       mutation SensorsAutoTarget($id: ID!, $target: Boolean!) {
         toggleSensorsAutoTarget(id: $id, target: $target)
       }
     `;
-    const sensors = this.props.sensors;
+    const sensors = sensors;
     const variables = {
       id: sensors.id,
       target: e.target.checked
     };
-    this.props.client.mutate({
+    client.mutate({
       mutation,
       variables
     });
   };
-  autoThrusters = e => {
+  const autoThrusters = e => {
     const mutation = gql`
       mutation SensorsAutoThrusters($id: ID!, $thrusters: Boolean!) {
         toggleSensorsAutoThrusters(id: $id, thrusters: $thrusters)
       }
     `;
-    const sensors = this.props.sensors;
+    const sensors = sensors;
     const variables = {
       id: sensors.id,
       thrusters: e.target.checked
     };
-    this.props.client.mutate({
+    client.mutate({
       mutation,
       variables
     });
   };
-  updateInterference = e => {
+  const updateInterference = e => {
     const mutation = gql`
       mutation SensorsInterference($id: ID!, $interference: Float!) {
         setSensorsInterference(id: $id, interference: $interference)
       }
     `;
-    const sensors = this.props.sensors;
+    const sensors = sensors;
     const variables = {
       id: sensors.id,
       interference: parseFloat(e.target.value)
     };
-    this.props.client.mutate({
+    client.mutate({
       mutation,
       variables
     });
   };
-  render() {
-    const {
-      sensors,
-      askForSpeed,
-      updateAskForSpeed,
-      client,
-      speed,
-      dragStart
-    } = this.props;
 
-    return (
-      <div className="sensors-extras">
-        <Nudge sensor={sensors.id} client={client} speed={speed} />
-        <div>
-          <label>
-            Ask for speed{" "}
-            <input
-              type="checkbox"
-              checked={askForSpeed}
-              onChange={evt => {
-                updateAskForSpeed(evt.target.checked);
-                localStorage.setItem(
-                  "thorium-core-sensors-askforspeed",
-                  evt.target.checked ? "yes" : "no"
-                );
-              }}
-            />
-          </label>
-        </div>
+  return (
+    <div className="sensors-extras">
+      <Nudge sensor={sensors.id} client={client} speed={speed} />
+      <div>
         <label>
-          Add to targeting{" "}
+          Ask for speed{" "}
           <input
             type="checkbox"
-            checked={sensors.autoTarget}
-            onChange={this.autoTarget}
+            checked={askForSpeed}
+            onChange={evt => {
+              updateAskForSpeed(evt.target.checked);
+              localStorage.setItem(
+                "thorium-core-sensors-askforspeed",
+                evt.target.checked ? "yes" : "no"
+              );
+            }}
           />
         </label>
-        <label>
-          Auto Thrusters{" "}
-          <input
-            type="checkbox"
-            checked={sensors.autoThrusters}
-            onChange={this.autoThrusters}
-          />
-        </label>
-        <small>Option-click grid segments to black out</small>
-        <Row>
-          <Col sm={10}>
-            <label>Planet</label>
-            <TypingField
-              input
-              controlled
-              value={this.state.planetLabel}
-              onChange={e => this.setState({ planetLabel: e.target.value })}
-            />
-            <input
-              type="range"
-              min={0.01}
-              max={2}
-              step={0.01}
-              value={this.state.planetSize}
-              onChange={e => this.setState({ planetSize: e.target.value })}
-            />
-
-            <ColorPicker
-              color={this.state.planetColor}
-              onChangeComplete={color => this.setState({ planetColor: color })}
-            />
-          </Col>
-          <Col sm={2}>
-            <div
-              className="planet-dragger"
-              onMouseDown={() =>
-                dragStart({
-                  color: this.state.planetColor,
-                  type: "planet",
-                  size: this.state.planetSize,
-                  name: this.state.planetLabel
-                })
-              }
-              style={{
-                borderColor: tinycolor(this.state.planetColor)
-                  .darken(10)
-                  .toString(),
-                backgroundColor: tinycolor(this.state.planetColor).toString()
-              }}
-            />
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col sm={10}>
-            <label>Border</label>
-            <TypingField
-              input
-              controlled
-              value={this.state.borderLabel}
-              onChange={e => this.setState({ borderLabel: e.target.value })}
-            />
-            <input
-              type="range"
-              min={0.01}
-              max={15}
-              step={0.01}
-              value={this.state.borderSize}
-              onChange={e => this.setState({ borderSize: e.target.value })}
-            />
-            <ColorPicker
-              color={this.state.borderColor}
-              onChangeComplete={color => this.setState({ borderColor: color })}
-            />
-          </Col>
-          <Col sm={2}>
-            <div
-              className="border-dragger"
-              onMouseDown={() =>
-                dragStart({
-                  color: this.state.borderColor,
-                  type: "border",
-                  size: this.state.borderSize,
-                  name: this.state.borderLabel
-                })
-              }
-              style={{
-                borderColor: tinycolor(this.state.borderColor)
-                  .darken(10)
-                  .toString(),
-                backgroundColor: tinycolor(this.state.borderColor).toString()
-              }}
-            />
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col sm={10}>
-            <label>Pings</label>
-            <input
-              type="range"
-              min={0.01}
-              max={2}
-              step={0.01}
-              value={this.state.pingSize}
-              onChange={e => this.setState({ pingSize: e.target.value })}
-            />
-            <ColorPicker
-              color={this.state.pingColor}
-              onChangeComplete={color => this.setState({ pingColor: color })}
-            />
-          </Col>
-          <Col sm={2}>
-            <div
-              className="pings-dragger"
-              onMouseDown={() =>
-                dragStart({
-                  color: this.state.pingColor,
-                  type: "ping",
-                  size: this.state.pingSize
-                })
-              }
-              style={{
-                borderColor: tinycolor(this.state.pingColor).toString(),
-                boxShadow: `inset 0px 0px 10px ${tinycolor(
-                  this.state.pingColor
-                ).toString()}`
-              }}
-            />
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col sm={12}>
-            <label>Interference</label>
-            <input
-              type="range"
-              min={0}
-              max={1}
-              step={0.01}
-              defaultValue={sensors.interference}
-              onMouseUp={this.updateInterference}
-            />
-          </Col>
-        </Row>
       </div>
-    );
-  }
-}
+      <label>
+        Add to targeting{" "}
+        <input
+          type="checkbox"
+          checked={sensors.autoTarget}
+          onChange={autoTarget}
+        />
+      </label>
+      <label>
+        Auto Thrusters{" "}
+        <input
+          type="checkbox"
+          checked={sensors.autoThrusters}
+          onChange={autoThrusters}
+        />
+      </label>
+      <small>Option-click grid segments to black out</small>
+      <Row>
+        <Col sm={10}>
+          <label>Planet</label>
+          <TypingField
+            input
+            controlled
+            value={planetLabel}
+            onChange={e => setPlanetLabel(e.target.value)}
+          />
+          <input
+            type="range"
+            min={0.01}
+            max={2}
+            step={0.01}
+            value={planetSize}
+            onChange={e => setPlanetSize(e.target.value)}
+          />
+
+          <ColorPicker
+            color={planetColor}
+            onChangeComplete={color =>
+              setPlanetColor(tinycolor(color).toString())
+            }
+          />
+        </Col>
+        <Col sm={2}>
+          <div
+            className="planet-dragger"
+            onMouseDown={() =>
+              dragStart({
+                color: planetColor,
+                type: "planet",
+                size: planetSize,
+                name: planetLabel
+              })
+            }
+            style={{
+              borderColor: tinycolor(planetColor)
+                .darken(10)
+                .toString(),
+              backgroundColor: tinycolor(planetColor).toString()
+            }}
+          />
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col sm={10}>
+          <label>Border</label>
+          <TypingField
+            input
+            controlled
+            value={borderLabel}
+            onChange={e => setBorderLabel(e.target.value)}
+          />
+          <input
+            type="range"
+            min={0.01}
+            max={15}
+            step={0.01}
+            value={borderSize}
+            onChange={e => setBorderSize(e.target.value)}
+          />
+          <ColorPicker
+            color={borderColor}
+            onChangeComplete={color =>
+              setBorderColor(tinycolor(color).toString())
+            }
+          />
+        </Col>
+        <Col sm={2}>
+          <div
+            className="border-dragger"
+            onMouseDown={() =>
+              dragStart({
+                color: borderColor,
+                type: "border",
+                size: borderSize,
+                name: borderLabel
+              })
+            }
+            style={{
+              borderColor: tinycolor(borderColor)
+                .darken(10)
+                .toString(),
+              backgroundColor: tinycolor(borderColor).toString()
+            }}
+          />
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col sm={10}>
+          <label>Pings</label>
+          <input
+            type="range"
+            min={0.01}
+            max={2}
+            step={0.01}
+            value={pingSize}
+            onChange={e => setPingSize(e.target.value)}
+          />
+          <ColorPicker
+            color={pingColor}
+            onChangeComplete={color =>
+              setPingColor(tinycolor(color).toString())
+            }
+          />
+        </Col>
+        <Col sm={2}>
+          <div
+            className="pings-dragger"
+            onMouseDown={() =>
+              dragStart({
+                color: pingColor,
+                type: "ping",
+                size: pingSize
+              })
+            }
+            style={{
+              borderColor: tinycolor(pingColor).toString(),
+              boxShadow: `inset 0px 0px 10px ${tinycolor(pingColor).toString()}`
+            }}
+          />
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col sm={12}>
+          <label>Interference</label>
+          <input
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+            defaultValue={sensors.interference}
+            onMouseUp={updateInterference}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default ExtraControls;
