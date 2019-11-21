deab98a721d21f69afde52dbcab5f415d5fa33db
Alex Anderson <raanderson@instructure.com>
2019-11-20 20:08:13 -0700

    fix(EpsilonLayout): Fixes strange card hiliting in the card switcher. Closes #2596

--- src/components/layouts/LayoutEpsilon/cardSwitcher.js
+++ src/components/layouts/LayoutEpsilon/cardSwitcher.js
@@ -1,67 +1,61 @@
-import React, {Component} from "react";
+import React from "react";
+import useOnClickOutside from "helpers/hooks/useClickOutside";
 
 const spregex = /.*.{8}-.{4}-.{4}-.{4}-.{12}/gi;
 const intregex = /interface-id:.{8}-.{4}-.{4}-.{4}-.{12}/gi;
-class CardSwitcher extends Component {
-  state = {};
-  render() {
-    const {cards, cardName, changeCard, hyperCard} = this.props;
-    const currentCard = cards.find(c => c.name === cardName) || cards[0];
-    const iconName = currentCard.component.match(intregex)
-      ? "Interface"
-      : currentCard.component.match(spregex)
-      ? "SoftwarePanels"
-      : currentCard.component;
-    const {shown} = this.state;
-    return (
-      <div
-        className={`navigation ${hyperCard ? "hypercard" : ""}`}
-        onClick={() => this.setState({shown: !shown})}
-      >
-        <h1 className="card-name">
-          <img
-            alt="card icon"
-            className="card-icon"
-            src={`/cardIcons/${iconName}.svg`}
-          />
-          {cardName}
-        </h1>
-        <div className="cards">
-          <div
-            className="cards-holder"
-            style={{display: shown ? `block` : `none`}}
-          >
-            {(cards || []).map(c => {
-              const cardName = c.component.match(spregex)
-                ? "SoftwarePanels"
-                : c.component.match(intregex)
-                ? "Interface"
-                : c.component;
-              return (
-                <div
-                  key={`card-${c.name}`}
-                  className={
-                    "navigation-card" + (cardName === c.name ? " active" : "")
-                  }
-                  onClick={() => {
-                    this.setState({shown: false});
-                    changeCard(c.name);
-                  }}
-                >
-                  <img
-                    alt="card icon"
-                    className="card-icon"
-                    src={`/cardIcons/${cardName}.svg`}
-                  />
-                  {c.name}
-                </div>
-              );
-            })}
-          </div>
+
+const CardSwitcher = ({cards, cardName, changeCard, hyperCard}) => {
+  const [shown, setShown] = React.useState();
+  const divRef = React.useRef();
+  useOnClickOutside(divRef, () => setShown(false));
+  const currentCard = cards.find(c => c.name === cardName) || cards[0];
+  const iconName = currentCard.component.match(intregex)
+    ? "Interface"
+    : currentCard.component.match(spregex)
+    ? "SoftwarePanels"
+    : currentCard.component;
+  const iconSrc = `/cardIcons/${iconName}.svg`;
+  return (
+    <div ref={divRef} className={`navigation ${hyperCard ? "hypercard" : ""}`}>
+      <h1 className="card-name" onClick={() => setShown(s => !s)}>
+        <img alt="card icon" className="card-icon" src={iconSrc} />
+        {cardName}
+      </h1>
+      <div className="cards">
+        <div
+          className="cards-holder"
+          style={{display: shown ? `block` : `none`}}
+        >
+          {(cards || []).map(c => {
+            const cardName = c.component.match(spregex)
+              ? "SoftwarePanels"
+              : c.component.match(intregex)
+              ? "Interface"
+              : c.component;
+
+            // Weird formatting here because of some strange
+            // syntax hiliting
+            const cardSrc = `/cardIcons/${cardName}.${"svg"}`;
+            return (
+              <div
+                key={`card-${c.name}`}
+                className={`navigation-card ${
+                  currentCard.component === cardName ? " active" : ""
+                }`}
+                onClick={() => {
+                  setShown(false);
+                  changeCard(c.name);
+                }}
+              >
+                <img alt="card icon" className="card-icon" src={cardSrc} />
+                {c.name}
+              </div>
+            );
+          })}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CardSwitcher;
