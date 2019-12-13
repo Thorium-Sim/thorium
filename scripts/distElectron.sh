2193792ea481547ffef4fbd98714243223abfa55
Alex Anderson <raanderson@instructure.com>
2019-12-13 09:30:43 -0700

    Lets see if we can get automatic deployment working.

---
+++ scripts/distElectron.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  docker run --rm \
+    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
+    -v ${PWD}:/project \
+    -v ~/.cache/electron:/root/.cache/electron \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+    electronuserland/builder:wine \
+    /bin/bash -c "yarn --link-duplicates --pure-lockfile && ./node_modules/.bin/electron-builder -p --linux --win"
+else
+  ./node_modules/.bin/electron-builder --mac -p
+fi
