8de981788decc8d781833574b690e73a1e56035f
Alex Anderson <raanderson@instructure.com>
2019-12-19 19:50:32 -0700

    fix(Sidebar): Fixes an issue on the core sidebar where it sometimes reverts. Closes #2576

--- src/components/core/sidebar.js
+++ src/components/core/sidebar.js
@@ -3,6 +3,7 @@ import "./sidebar.scss";
 import {Cores} from "components/views";
 import {subscribe} from "helpers/pubsub";
 import CoreError from "./layouts/coreError";
+import useLocalStorage from "helpers/hooks/useLocalStorage";
 
 const defaultSidebar = [
   {
@@ -26,116 +27,100 @@ const defaultSidebar = [
   },
 ];
 
-class Sidebar extends Component {
-  state = {
-    sidebar: localStorage.getItem("thorium_coreSidebarItems")
-      ? JSON.parse(localStorage.getItem("thorium_coreSidebarItems"))
-      : defaultSidebar,
-    positions: {},
-    open: {},
-    showing: {},
-  };
-  componentDidMount() {
-    this.sub = subscribe("core_sidebar_update", () => {
-      this.setState({
-        sidebar: localStorage.getItem("thorium_coreSidebarItems")
-          ? JSON.parse(localStorage.getItem("thorium_coreSidebarItems"))
-          : defaultSidebar,
-      });
+const Sidebar = props => {
+  const [sidebar, setSidebar] = useLocalStorage(
+    "thorium_coreSidebarItems",
+    defaultSidebar,
+  );
+  const [positions, setPositions] = React.useState({});
+  const [open, setOpen] = React.useState({});
+  const [showing, setShowing] = React.useState({});
+
+  React.useEffect(() => {
+    return subscribe("core_sidebar_update", () => {
+      setSidebar(JSON.parse(localStorage.getItem("thorium_coreSidebarItems")));
     });
-  }
-  updateShowing = () => {
-    this.setState(state => ({
-      showing: state.open,
-    }));
-  };
-  render() {
-    const {sidebar, positions, open, showing} = this.state;
-    return (
-      <div className="core-sidebar">
-        <div className="labels">
-          {sidebar.map(s => (
-            <div
-              ref={el => {
-                if (el && !this.state.positions[`${s.label}-${s.component}`]) {
-                  const bounds = el.getBoundingClientRect();
-                  this.setState(state => ({
-                    positions: {
-                      ...state.positions,
-                      [`${s.label}-${s.component}`]: {
-                        top: bounds.top,
-                        bottom: bounds.bottom,
-                      },
-                    },
-                  }));
-                }
-              }}
-              className="sidebar-label"
-              key={`${s.label}-${s.component}`}
-              onClick={() =>
-                this.setState(
-                  state => ({
-                    open: {
-                      [`${s.label}-${s.component}`]: !open[
-                        `${s.label}-${s.component}`
-                      ],
-                    },
-                    showing: {
-                      ...state.showing,
-                      [`${s.label}-${s.component}`]: true,
-                    },
-                  }),
-                  () => setTimeout(this.updateShowing, 500),
-                )
+  }, [setSidebar]);
+
+  React.useEffect(() => {
+    setShowing(open);
+  }, [open]);
+  return (
+    <div className="core-sidebar">
+      <div className="labels">
+        {sidebar.map(s => (
+          <div
+            ref={el => {
+              if (el && !positions[`${s.label}-${s.component}`]) {
+                const bounds = el.getBoundingClientRect();
+                setPositions(p => ({
+                  ...p,
+                  [`${s.label}-${s.component}`]: {
+                    top: bounds.top,
+                    bottom: bounds.bottom,
+                  },
+                }));
               }
+            }}
+            className="sidebar-label"
+            key={`${s.label}-${s.component}`}
+            onClick={() => {
+              setOpen({
+                [`${s.label}-${s.component}`]: !open[
+                  `${s.label}-${s.component}`
+                ],
+              });
+              setShowing(showing => ({
+                ...showing,
+                [`${s.label}-${s.component}`]: true,
+              }));
+            }}
+            style={{
+              transform: `rotate(180deg) translate(0, ${
+                open[`${s.label}-${s.component}`] ? "-350px" : "0"
+              })`,
+            }}
+          >
+            {s.label}
+          </div>
+        ))}
+      </div>
+      <div className="sidebar-components">
+        {sidebar.map(s => {
+          if (!positions[`${s.label}-${s.component}`]) return null;
+          const onBottom =
+            window.innerHeight <
+            positions[`${s.label}-${s.component}`].top + 300;
+          const position = onBottom
+            ? positions[`${s.label}-${s.component}`].bottom - window.innerHeight
+            : positions[`${s.label}-${s.component}`].top;
+          return (
+            <div
+              key={`component-${s.label}-${s.component}`}
+              className={"sidebar-component"}
               style={{
-                transform: `rotate(180deg) translate(0, ${
+                bottom: onBottom ? 0 : null,
+                top: onBottom ? null : 0,
+                transform: `translate(${
                   open[`${s.label}-${s.component}`] ? "-350px" : "0"
-                })`,
+                }, ${position}px)`,
               }}
             >
-              {s.label}
+              {showing[`${s.label}-${s.component}`] &&
+                (() => {
+                  const Comp = Cores[s.component];
+                  if (!Comp) return null;
+                  return (
+                    <CoreError>
+                      <Comp {...props} />
+                    </CoreError>
+                  );
+                })()}
             </div>
-          ))}
-        </div>
-        <div className="sidebar-components">
-          {sidebar.map(s => {
-            if (!positions[`${s.label}-${s.component}`]) return null;
-            const onBottom =
-              window.innerHeight <
-              positions[`${s.label}-${s.component}`].top + 300;
-            const position = onBottom
-              ? positions[`${s.label}-${s.component}`].bottom -
-                window.innerHeight
-              : positions[`${s.label}-${s.component}`].top;
-            return (
-              <div
-                key={`component-${s.label}-${s.component}`}
-                className={"sidebar-component"}
-                style={{
-                  bottom: onBottom ? 0 : null,
-                  top: onBottom ? null : 0,
-                  transform: `translate(${
-                    open[`${s.label}-${s.component}`] ? "-350px" : "0"
-                  }, ${position}px)`,
-                }}
-              >
-                {showing[`${s.label}-${s.component}`] &&
-                  (() => {
-                    const Comp = Cores[s.component];
-                    if (!Comp) return null;
-                    return (
-                      <CoreError>
-                        <Comp {...this.props} />
-                      </CoreError>
-                    );
-                  })()}
-              </div>
-            );
-          })}
-        </div>
+          );
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 export default Sidebar;
