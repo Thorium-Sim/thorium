fc93895176fb86c617e932c6464182e80398f6af
Alex Anderson <raanderson@instructure.com>
2019-12-30 13:10:05 -0700

    fix(Sensors): Fixes the creation of duplicate sensor contacts when dragging one to the screen. Closes #2675

--- src/components/views/Sensors/gridCore/hooks/useDragStart.js
+++ src/components/views/Sensors/gridCore/hooks/useDragStart.js
@@ -12,6 +12,8 @@ export function useDragStart({id}, dimensions, addContact) {
   const [createContact] = useMutation(CREATE_CONTACT);
   const [movingContact, setMovingContact] = React.useState(null);
   const hasMovingContact = Boolean(movingContact);
+  const mover = React.useRef({});
+
   React.useEffect(() => {
     function dragMove(evt) {
       const {clientX, clientY} = evt;
@@ -36,13 +38,19 @@ export function useDragStart({id}, dimensions, addContact) {
             0.08,
           z: 0,
         };
-        return c ? {...c, location: destination, destination} : null;
+        mover.current = c
+          ? {...c, location: destination, destination: destination}
+          : null;
+        return c
+          ? {...c, location: destination, destination: destination}
+          : null;
       });
     }
     function dragUp() {
       document.removeEventListener("mousemove", dragMove);
       document.removeEventListener("mouseup", dragUp);
       if (!hasMovingContact) return;
+
       setMovingContact(c => {
         const {location, type, size} = c;
         if (!location) return c;
@@ -51,23 +59,31 @@ export function useDragStart({id}, dimensions, addContact) {
         if (distance > maxDistance) {
           return c;
         }
-        const {id: contactId, ...contactVariables} = c;
-        addContact({
-          ...contactVariables,
-          size: parseFloat(size),
-          destination: location,
-        });
-        createContact({
-          variables: {
-            id,
-            contact: {
-              ...contactVariables,
-              size: parseFloat(size),
-              destination: location,
-            },
-          },
-        });
         return null;
+      });
+      // Remove contact ID so we don't create duplicates
+      const {
+        id: contactId,
+        size,
+        location,
+        ...contactVariables
+      } = mover.current;
+      addContact({
+        ...contactVariables,
+        size: parseFloat(size),
+        destination: location,
+        location,
+      });
+      createContact({
+        variables: {
+          id,
+          contact: {
+            ...contactVariables,
+            size: parseFloat(size),
+            destination: location,
+            location,
+          },
+        },
       });
     }
 
