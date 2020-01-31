92418e6eaecbc03933fb2ad9d7533eae3baa644d
Alex Anderson <raanderson@instructure.com>
2020-01-30 17:25:43 -0700

    Finished up the countermeasure class.

--- server/classes/Countermeasure.ts
+++ server/classes/Countermeasure.ts
@@ -7,6 +7,7 @@ class CountermeasureResources {
   carbon?: number;
   plastic?: number;
   plasma?: number;
+  [key: string]: number;
   constructor({copper = 0, titanium = 0, carbon = 0, plastic = 0, plasma = 0}) {
     this.copper = copper;
     this.titanium = titanium;
@@ -16,118 +17,180 @@ class CountermeasureResources {
   }
 }
 
+interface CountermeasureModuleParams {
+  id?: string;
+  name: string;
+  description: string;
+  powerRequirement?: number;
+  resourceRequirements: CountermeasureResources;
+  configurationOptions?: {type: string; label: string}[];
+  config?: any;
+  buildProgress?: number;
+  activated?: boolean;
+}
+
 class CountermeasureModule {
   id: string;
   name: string;
   description: string;
   powerRequirement: number;
   resourceRequirements: CountermeasureResources;
-  constructor(
-    name: string,
-    description: string,
-    resourceRequirements: CountermeasureResources,
-    powerRequirement: number = 0,
-  ) {
-    this.id = snakeCase(name);
+  configurationOptions?: {type: string; label: string}[];
+  config?: any;
+  buildProgress?: number;
+  activated?: boolean;
+  constructor({
+    id,
+    name,
+    description,
+    resourceRequirements,
+    powerRequirement = 0,
+    configurationOptions = [],
+    config = {},
+    buildProgress = 0,
+    activated = false,
+  }: CountermeasureModuleParams) {
+    this.id = id || snakeCase(name);
     this.name = name;
     this.description = description;
     this.powerRequirement = powerRequirement;
     this.resourceRequirements = new CountermeasureResources(
       resourceRequirements,
     );
+    this.configurationOptions = configurationOptions;
+    this.config = config;
+    this.buildProgress = buildProgress;
+    this.activated = activated;
+  }
+  activate() {
+    this.activated = true;
   }
 }
-const moduleTypes = [
-  new CountermeasureModule(
-    "Heat Coil",
-    "Generates heat. Can be configured to pulse or produce a constant heat signature.",
-    {copper: 3, carbon: 2},
-    1,
-  ),
-  new CountermeasureModule(
-    "Explosive Payload",
-    "When activated, it explodes. Do not activate immediately upon launch.",
-    {copper: 2, plasma: 3},
-  ),
-  new CountermeasureModule(
-    "Communications Array",
-    "Broadcasts a repeating signal at a pre-defined frequency. The message in the signal can be pre-configured.",
-    {copper: 2, titanium: 1, plastic: 1},
-    0.25,
-  ),
-  new CountermeasureModule(
-    "Battery Cell",
-    "Provides power to other modules. More battery cells provide longer use of modules.",
-    {carbon: 1, titanium: 2, plasma: 3},
-  ),
-  new CountermeasureModule(
-    "Sensor Scrambler",
-    "Scrambles signals on the sensor grid. Can also be used to disrupt communication signals.",
-    {copper: 1, plasma: 2, plastic: 1},
-    0.5,
-  ),
-  new CountermeasureModule(
-    "Transport Inhibitor",
-    "Blocks or limits the ability of nearby ships to use transporter systems.",
-    {copper: 1, plasma: 2, titanium: 3},
-    0.75,
-  ),
-  new CountermeasureModule(
-    "Ionizer",
-    "Creates a cloud of particles which simulates the trail left by a small starship.",
-    {plasma: 1, carbon: 1, titanium: 1},
-    0.25,
-  ),
-  new CountermeasureModule(
-    "Proximity Detector",
-    "Detects when ships approach. Other modules can use this to trigger their activation.",
-    {copper: 1, titanium: 1, plastic: 3},
-    0.25,
-  ),
-  new CountermeasureModule(
-    "Scan Detector",
-    "Detects when the module is scanned. Other modules can use this to trigger their activation.",
-    {copper: 1, titanium: 1, plastic: 3},
-    0.25,
-  ),
-  new CountermeasureModule(
-    "Beacon",
-    "Sends out a pulse which can be used by navigation to locate the module.",
-    {plasma: 1, carbon: 1, copper: 2},
-    0.5,
-  ),
-  new CountermeasureModule(
-    "Notifier",
-    "Uses encrypted, entangled quantum particles to securely send a message back to your station.",
-    {plasma: 1, carbon: 1, titanium: 1, copper: 1},
-  ),
+
+export const moduleTypes = [
+  new CountermeasureModule({
+    name: "Heat Coil",
+    description:
+      "Generates heat. Can be configured to pulse or produce a constant heat signature. This can mimic the heat signature of a ship.",
+    resourceRequirements: {copper: 3, carbon: 2},
+    powerRequirement: 1,
+    configurationOptions: [
+      {type: "Pulse:Constant", label: "Mode"},
+      {type: "Trigger", label: "Trigger"},
+    ],
+  }),
+  new CountermeasureModule({
+    name: "Explosive Payload",
+    description:
+      "When activated, it explodes. Do not activate immediately upon launch.",
+    resourceRequirements: {copper: 2, plasma: 3},
+    configurationOptions: [{type: "Trigger", label: "Trigger"}],
+  }),
+  new CountermeasureModule({
+    name: "Communications Array",
+    description:
+      "Broadcasts a repeating signal at a pre-defined frequency. The message in the signal can be pre-configured.",
+    resourceRequirements: {copper: 2, titanium: 1, plastic: 1},
+    powerRequirement: 0.25,
+    configurationOptions: [
+      {type: "Number", label: "Frequency"},
+      {type: "TextArea", label: "Message"},
+      {type: "Trigger", label: "Trigger"},
+    ],
+  }),
+  new CountermeasureModule({
+    name: "Battery Cell",
+    description:
+      "Provides power to other modules. More battery cells provide longer use of modules.",
+    resourceRequirements: {carbon: 1, titanium: 2, plasma: 3},
+    configurationOptions: [{type: "Trigger", label: "Trigger"}],
+  }),
+  new CountermeasureModule({
+    name: "Sensor Scrambler",
+    description:
+      "Scrambles signals on the sensor grid. Can also be used to disrupt communication signals.",
+    resourceRequirements: {copper: 1, plasma: 2, plastic: 1},
+    powerRequirement: 0.5,
+    configurationOptions: [{type: "Trigger", label: "Trigger"}],
+  }),
+  new CountermeasureModule({
+    name: "Transport Inhibitor",
+    description:
+      "Blocks or limits the ability of nearby ships to use transporter systems.",
+    resourceRequirements: {copper: 1, plasma: 2, titanium: 3},
+    powerRequirement: 0.75,
+    configurationOptions: [{type: "Trigger", label: "Trigger"}],
+  }),
+  new CountermeasureModule({
+    name: "Ionizer",
+    description:
+      "Creates a cloud of particles which simulates the trail left by a small starship.",
+    resourceRequirements: {plasma: 1, carbon: 1, titanium: 1},
+    powerRequirement: 0.25,
+    configurationOptions: [{type: "Trigger", label: "Trigger"}],
+  }),
+  new CountermeasureModule({
+    name: "Proximity Trigger",
+    description:
+      "Detects when ships approach. Other modules can use this to trigger their activation.",
+    resourceRequirements: {copper: 1, titanium: 1, plastic: 3},
+    powerRequirement: 0.25,
+  }),
+  new CountermeasureModule({
+    name: "Scan Trigger",
+    description:
+      "Detects when the module is scanned. Other modules can use this to trigger their activation.",
+    resourceRequirements: {copper: 1, titanium: 1, plastic: 3},
+    powerRequirement: 0.25,
+  }),
+  new CountermeasureModule({
+    name: "Beacon",
+    description:
+      "Sends out a pulse which can be used by navigation to locate the module.",
+    resourceRequirements: {plasma: 1, carbon: 1, copper: 2},
+    powerRequirement: 0.5,
+    configurationOptions: [{type: "Trigger", label: "Trigger"}],
+  }),
+  new CountermeasureModule({
+    name: "Notifier",
+    description:
+      "Uses encrypted, entangled quantum particles to securely send a message back to your station.",
+    resourceRequirements: {plasma: 1, carbon: 1, titanium: 1, copper: 1},
+    configurationOptions: [
+      {type: "Trigger", label: "Trigger"},
+      {type: "TextArea", label: "Message"},
+    ],
+  }),
 ];
 
 class CountermeasureSlots {
-  slot1?: Countermeasure;
-  slot2?: Countermeasure;
-  slot3?: Countermeasure;
-  slot4?: Countermeasure;
-  slot5?: Countermeasure;
-  slot6?: Countermeasure;
-  slot7?: Countermeasure;
-  slot8?: Countermeasure;
+  slot1: Countermeasure | null;
+  slot2: Countermeasure | null;
+  slot3: Countermeasure | null;
+  slot4: Countermeasure | null;
+  slot5: Countermeasure | null;
+  slot6: Countermeasure | null;
+  slot7: Countermeasure | null;
+  slot8: Countermeasure | null;
+  [key: string]: Countermeasure | null;
   constructor(params: CountermeasureSlots) {
-    this.slot1 = params.slot1;
-    this.slot2 = params.slot2;
-    this.slot3 = params.slot3;
-    this.slot4 = params.slot4;
-    this.slot5 = params.slot5;
-    this.slot6 = params.slot6;
-    this.slot7 = params.slot7;
-    this.slot8 = params.slot8;
+    this.slot1 = params.slot1 ? new Countermeasure(params.slot1) : null;
+    this.slot2 = params.slot2 ? new Countermeasure(params.slot2) : null;
+    this.slot3 = params.slot3 ? new Countermeasure(params.slot3) : null;
+    this.slot4 = params.slot4 ? new Countermeasure(params.slot4) : null;
+    this.slot5 = params.slot5 ? new Countermeasure(params.slot5) : null;
+    this.slot6 = params.slot6 ? new Countermeasure(params.slot6) : null;
+    this.slot7 = params.slot7 ? new Countermeasure(params.slot7) : null;
+    this.slot8 = params.slot8 ? new Countermeasure(params.slot8) : null;
   }
 }
+
 interface DefaultParams {
   materials?: CountermeasureResources;
   slots?: CountermeasureSlots;
   launched?: Countermeasure[];
 }
+
 export class Countermeasures extends System {
   materials: CountermeasureResources;
   slots: CountermeasureSlots;
@@ -149,8 +212,114 @@ export class Countermeasures extends System {
     this.launched = [];
     params.launched?.forEach(l => this.launched.push(new Countermeasure(l)));
   }
+  addCountermeasure(slot: string, name: string) {
+    this.slots[slot] = new Countermeasure({id: uuid.v4(), name});
+  }
+  removeCountermeasure(slot) {
+    if (this.slots[slot]) {
+      const countermeasure: Countermeasure = this.slots[slot];
+      this.slots[slot] = null;
+
+      // Remove all of the unused materials from the countermeasure modules
+      countermeasure.modules.forEach(m => {
+        const leftoverMaterialValue = 1 - m.buildProgress;
+        Object.entries(m.resourceRequirements).forEach(([key, value]) => {
+          this.materials[key] += leftoverMaterialValue * value;
+        });
+      });
+    }
+  }
+  launchCountermeasure(slot) {
+    if (this.slots[slot]?.readyToLaunch) {
+      this.launched.push(this.slots[slot]);
+      this.slots[slot] = null;
+    }
+  }
+  buildCountermeasure(slot) {
+    this.slots[slot]?.build();
+  }
+  addCountermeasureModule(slot, moduleType) {
+    this.slots[slot]?.addModule(moduleType);
+  }
+  removeCountermeasureModule(slot, id) {
+    this.slots[slot]?.removeModule(id);
+  }
+  configureCountermeasureModule(slot, id, config) {
+    this.slots[slot]?.configureModule(id, config);
+  }
 }
 
+interface CountermeasureParams {
+  id: string;
+  name: string;
+  modules?: CountermeasureModule[];
+  locked?: boolean;
+  active?: boolean;
+  building?: boolean;
+  totalPowerUsed?: number;
+}
 class Countermeasure {
-  constructor(params) {}
+  id: string;
+  name: string;
+  modules: CountermeasureModule[];
+  locked: boolean;
+  active: boolean;
+  building: boolean;
+  totalPowerUsed: number;
+  constructor(params: CountermeasureParams) {
+    this.id = params.id || uuid.v4();
+    this.name = params.name || "Countermeasure";
+    this.modules = [];
+    params.modules?.forEach(m =>
+      this.modules.push(new CountermeasureModule(m)),
+    );
+    this.locked = params.locked || false;
+    this.active = params.active || false;
+    this.building = params.building || false;
+    this.totalPowerUsed = params.totalPowerUsed || 0;
+  }
+
+  get readyToLaunch() {
+    // If we have a module and all of the modules are built, then the countermeasure is ready to launch.
+    return (
+      this.modules.length > 0 &&
+      this.modules.reduce((acc, next) => {
+        if (!acc) return false;
+        if (next.buildProgress < 1) return false;
+        return true;
+      }, true)
+    );
+  }
+  get powerUsage() {
+    return this.modules.reduce((acc, mod) => {
+      return acc + (mod.powerRequirement || 0);
+    }, 0);
+  }
+  get availablePower() {
+    // Each battery provides 1 unit of power for one unit of power usage.
+    // Countermeasures come with half a unit worth of power by default.
+    return this.modules.filter(f => f.name === "Battery Cell").length + 0.5;
+  }
+  build() {
+    this.building = true;
+  }
+
+  addModule(moduleType) {
+    if (!this.building) {
+      const mod = moduleTypes.find(m => m.name === moduleType);
+      this.modules.push(new CountermeasureModule({...mod, id: uuid.v4()}));
+    }
+  }
+  removeModule(id) {
+    this.modules = this.modules.filter(m => m.id !== id);
+  }
+  configureModule(id, config) {
+    const mod = this.modules.find(m => m.id === id);
+    if (mod) {
+      mod.config = config;
+    }
+  }
+  activateModule(id) {
+    this.modules.find(m => m.id === id)?.activate();
+  }
 }
