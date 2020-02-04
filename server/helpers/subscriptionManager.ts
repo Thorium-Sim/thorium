867c8d413a87bf54322e8168bbf9707654598550
Alex Anderson <raanderson@instructure.com>
2020-02-04 13:59:11 -0700

    Deploy change.

--- server/helpers/subscriptionManager.ts
+++ server/helpers/subscriptionManager.ts
@@ -1,7 +1,7 @@
 import {PubSub} from "apollo-server-express";
 import EventEmitter from "events";
 
-const ee = new EventEmitter();
+const ee = new EventEmitter.EventEmitter();
 ee.setMaxListeners(250);
 
 const pubsub = new PubSub({eventEmitter: ee});
