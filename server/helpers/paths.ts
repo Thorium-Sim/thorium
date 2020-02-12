3842f7299b448f41d72c19715ecfe65a4ae23cb7
Alex Anderson <raanderson@instructure.com>
2020-02-11 20:08:28 -0700

    feat(Advanced): Makes it possible to change the Thorium data directory with the THORIUM_PATH environment variable. Intended for advanced users. Closes #2757

--- server/helpers/paths.ts
+++ server/helpers/paths.ts
@@ -1,11 +1,26 @@
 import fs from "fs";
-
+import mkdirp from "mkdirp";
 if (!fs.existsSync(`${require("os").homedir()}/Documents`)) {
   fs.mkdirSync(`${require("os").homedir()}/Documents`);
 }
 
+let thoriumPath = `${require("os").homedir()}/Documents/thorium`;
+
+if (process.env.THORIUM_PATH) {
+  const testPath = String(process.env.THORIUM_PATH).replace(
+    "~",
+    require("os").homedir(),
+  );
+  try {
+    mkdirp.sync(testPath);
+    thoriumPath = testPath;
+  } catch {
+    // Do nothing.
+  }
+}
+
 const paths = {
-  userData: `${require("os").homedir()}/Documents/thorium`,
+  userData: thoriumPath,
 };
 
 export default paths;
