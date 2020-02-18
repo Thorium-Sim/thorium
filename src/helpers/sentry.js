d785ef87a381548f74650db0fbcb40ac27ffd414
Alex Anderson <raanderson@instructure.com>
2020-02-18 14:00:53 -0700

    Fix a sentry error

--- src/helpers/sentry.js
+++ src/helpers/sentry.js
@@ -10,10 +10,7 @@ if (process.env.NODE_ENV === "production") {
     release: `react@${version}`,
     dsn: "https://c71a50635f9b42f4a70880cce6a7ff0e@sentry.io/1323903",
     beforeSend(event) {
-      if (
-        event.exception.mechanism.data &&
-        invalidMessages.includes(event.exception.mechanism.data.message)
-      )
+      if (invalidMessages.includes(event?.exception?.mechanism?.data?.message))
         return null;
       return event;
     },
