67a249f508bbd7731e639f469f2bdd12f7e9fb55
Alex Anderson <alexanderson1993@gmail.com>
2020-12-02 15:16:47 -0500

    fix(DMX): Fixes a memory leak that causes the DMX system to slow down and crash after several minutes of use. Closes #3042

--- src/helpers/hooks/useLocalStorage.js
+++ src/helpers/hooks/useLocalStorage.js
@@ -16,20 +16,30 @@ export default function useLocalStorage(key, initialValue) {
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = value => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      // Save state
-      setStoredValue(valueToStore);
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.error(error);
-    }
-  };
+  const setValue = React.useCallback(
+    value => {
+      try {
+        // Allow value to be a function so we have same API as useState
+        // Save state
+        setStoredValue(storedValue => {
+          try {
+            const valueToStore =
+              value instanceof Function ? value(storedValue) : value;
+
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+            return valueToStore;
+          } catch {
+            return storedValue;
+          }
+        });
+        // Save to local storage
+      } catch (error) {
+        // A more advanced implementation would handle the error case
+        console.error(error);
+      }
+    },
+    [key],
+  );
 
   return [storedValue, setValue];
 }
