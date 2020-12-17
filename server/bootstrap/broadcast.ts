ad9c50ead398871953a144a5869612e55a27a910
Alex Anderson <alexanderson1993@gmail.com>
2020-12-17 08:10:21 -0500

    fix(Autoconnect): Fixes an issue where clients cannot find multiple servers connected on the same network.

--- server/bootstrap/broadcast.ts
+++ server/bootstrap/broadcast.ts
@@ -1,12 +1,17 @@
-const bonjour = require("bonjour")();
+const bonjour = require("bonjour")({reuseAddr: true});
 
 function publishBonjour(port = 4444, httpOnly: boolean, tryCount = 0) {
   try {
-    bonjour.publish({
+    const service = bonjour.publish({
       name: `Thorium${tryCount ? ` (${tryCount})` : ""}`,
       type: "http",
       port: port,
       txt: {https: String(process.env.NODE_ENV === "production" && !httpOnly)},
+    });
+    service.on("error", err => {
+      // Thorium is likely already running on the network. Do it again with a higher try count.
+      service.stop();
+      publishBonjour(port, httpOnly, tryCount + 1);
     });
   } catch {
     // Thorium is likely already running on the network. Do it again with a higher try count.
