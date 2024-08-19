285d0c6915e43b41fd4ae9f74201a8652d68c8b8
Mitchell Foote <mitchellfoote93@gmail.com>
2024-08-19 16:46:53 -0600

    fix(3357): switched to using the BrowserWindow API to get window objects

--- public/electron/helpers/hotkeys.js
+++ public/electron/helpers/hotkeys.js
@@ -1,9 +1,10 @@
-const {app, globalShortcut, BrowserWindow} = require("electron");
-const {windows} = require("./multiWindow");
-const {clearMenubar, setMenubar} = require("./setMenubar");
+const { app, globalShortcut, BrowserWindow } = require("electron");
+const { clearMenubar, setMenubar } = require("./setMenubar");
 
 module.exports = () => {
   globalShortcut.register("CommandOrControl+Alt+R", function () {
+    // This grabs all non-server windows, as the server window doesn't have a uniqueId.
+    const windows = BrowserWindow.getAllWindows().filter((each) => each.uniqueId);
     windows.forEach(mainWindow => {
       mainWindow && mainWindow.reload();
     });
@@ -14,6 +15,8 @@ module.exports = () => {
     focused && focused.webContents.openDevTools();
   });
   globalShortcut.register("CommandOrControl+Alt+K", function () {
+    // This grabs all non-server windows, as the server window doesn't have a uniqueId.
+    const windows = BrowserWindow.getAllWindows().filter((each) => each.uniqueId);
     if (windows[0] && windows[0].isKiosk()) {
       windows.forEach(mainWindow => {
         mainWindow.setKiosk(false);
