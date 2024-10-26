d16d1216dd7702ad565be91188ec6e3b4cadad94
Mitchell Foote <mitchellfoote93@gmail.com>
2024-10-26 12:09:21 -0600

    added advanced nav processes

---
+++ server/processes/advanced-nav.ts
@@ -0,0 +1,19 @@
+import AdvancedNavigationAndAstrometrics from "~classes/advancedNavigationAndAstrometrics";
+import App from "../app";
+import { pubsub } from "../helpers/subscriptionManager";
+
+const updateValues = () => {
+    App.flights.filter(f => f.running === true)
+        .forEach(f => {
+            f.simulators.forEach(s => {
+                App.systems.filter(sys => sys.simulatorId === s.id && sys.type === "AdvancedNavigationAndAstrometrics").forEach((nav: AdvancedNavigationAndAstrometrics) => {
+                    nav.executeHeatInterval();
+                    nav.executeLoopInterval();
+                    nav.executeProbeInterval();
+                })
+            });
+        });
+    setTimeout(updateValues, 1000);
+};
+updateValues();
+
