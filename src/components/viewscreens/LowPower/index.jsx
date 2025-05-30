ab671ca9537fcbf6197616c9e12409c44b3b0034
Alex Anderson <hey@ralexanderson.com>
2025-05-29 19:36:10 -0600

    fix: Addresses issues with certain viewscreens

--- src/components/viewscreens/LowPower/index.jsx
+++ src/components/viewscreens/LowPower/index.jsx
@@ -1,30 +1,14 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
-import bodymovin from "bodymovin";
+import Lottie from 'react-lottie-player'
+
 import animationData from "./data.json";
 
-export default class AsteroidField extends Component {
-  componentDidMount() {
-    bodymovin.loadAnimation({
-      container: ReactDOM.findDOMNode(this).querySelector("#bodymovin"),
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData,
-    });
-  }
-  render() {
-    return (
-      <div
-        style={{
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div id="bodymovin" style={{height: "80vh"}} />
-      </div>
-    );
-  }
+export default function LowPower() {
+   return <Lottie
+      loop
+      animationData={animationData}
+      play
+    />
 }
+
